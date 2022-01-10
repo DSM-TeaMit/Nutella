bb6ee68d9495840f49a29913350e47bef1b254c6
김진근 <freedom7113@gmail.com>
2022-01-10 15:06:03 +0900

    ➕️ (#8) - add theme.ts

---
+++ nutella/src/style/theme.ts
@@ -0,0 +1,43 @@
+const colors = {
+  primary: {
+    default: "#0094FF",
+    hover: "#66BFFF",
+    click: "#0076CC",
+  },
+  red: {
+    default: "#F56C6C",
+    hover: "#F59393",
+    click: "#B24F4F",
+  },
+  green: {
+    default: "#1CD0A1",
+  },
+  grayscale: {
+    white: "#FFFFFF",
+    lightGray1: "#F0F3F5",
+    lightGray2: "#E0E7EC",
+    gray1: "#B4BEC5",
+    gray2: "#717A81",
+    darkGray: "#494E52",
+    black: "#1C1E20",
+  },
+};
+
+const fonts = {
+  h1: `bold 2.25rem 'Noto Sans KR', 'sans-serif'`,
+  h2: `bold 1.75rem 'Noto Sans KR', 'sans-serif'`,
+  h3: `bold 1.5rem 'Noto Sans KR', 'sans-serif'`,
+  subtitle1: `500 1.25rem 'Noto Sans KR', 'sans-serif'`,
+  subtitle2: `500 1.125rem 'Noto Sans KR', 'sans-serif'`,
+  body1: `bold 1rem 'Noto Sans KR', 'sans-serif'`,
+  body2: `500 1rem 'Noto Sans KR', 'sans-serif'`,
+  body3: `400 1rem 'Noto Sans KR', 'sans-serif'`,
+  description: `400 0.75rem 'Noto Sans KR', 'sans-serif'`,
+};
+
+const theme = {
+  colors,
+  fonts,
+};
+
+export default theme;
