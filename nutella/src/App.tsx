121634675563015ae9f3d6c77733d6789fca0e35
김진근 <freedom7113@gmail.com>
2022-01-10 16:40:55 +0900

    :test_tube: :: (#8) add theme test

--- nutella/src/App.tsx
+++ nutella/src/App.tsx
@@ -1,10 +1,17 @@
 import { ThemeProvider } from "@emotion/react";
+import styled from "@emotion/styled";
 import theme from "./style/theme";
+
+const Test = styled.div`
+  font: ${(props) => props.theme.fonts.h1};
+  color: ${(props) => props.theme.colors.primary.default};
+`;
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div>hello world!</div>
+      <Test>Hello i'm theme test</Test>
     </ThemeProvider>
   );
 }
