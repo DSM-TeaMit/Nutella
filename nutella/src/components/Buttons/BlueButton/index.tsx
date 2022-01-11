f695b1b977afa858232ced935e6c641394afe507
김진근 <freedom7113@gmail.com>
2022-01-11 12:43:04 +0900

    🍫 (#11) - add button props

--- nutella/src/components/Buttons/BlueButton/index.tsx
+++ nutella/src/components/Buttons/BlueButton/index.tsx
@@ -1,8 +1,8 @@
 import { FC } from "react";
 import { Button } from "./styles";
 
-const BlueButton: FC = ({ children }) => {
-  return <Button>{children}</Button>;
+const BlueButton: FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = (props) => {
+  return <Button {...props} />;
 };
 
 export default BlueButton;
