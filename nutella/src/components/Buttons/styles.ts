f2839143bc77ef4d0ead4bb59715ff8434d8d8fd
김진근 <freedom7113@gmail.com>
2022-01-11 12:37:55 +0900

    🍫 (#11) - add border radius

--- nutella/src/components/Buttons/styles.ts
+++ nutella/src/components/Buttons/styles.ts
@@ -3,4 +3,5 @@ import styled from "@emotion/styled";
 export const DefaultButtonStyle = styled.button`
   padding: 8px 16px;
   font: ${({ theme }) => theme.fonts.body2};
+  border-radius: 10px;
 `;
