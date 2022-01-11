d28e774090468312248da87b9f6887fb70891e6f
김진근 <freedom7113@gmail.com>
2022-01-11 12:47:51 +0900

    🍫 (#11) - add various state style

--- nutella/src/components/Buttons/BlueButton/styles.ts
+++ nutella/src/components/Buttons/BlueButton/styles.ts
@@ -5,4 +5,17 @@ export const Button = styled(DefaultButtonStyle)`
   border: 0;
   background-color: ${({ theme }) => theme.colors.primary.default};
   color: ${({ theme }) => theme.colors.grayscale.white};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.primary.hover};
+  }
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.colors.primary.click};
+    cursor: default;
+  }
+
+  &:active {
+    background-color: ${({ theme }) => theme.colors.primary.click};
+  }
 `;
--- nutella/src/components/Buttons/styles.ts
+++ nutella/src/components/Buttons/styles.ts
@@ -4,4 +4,5 @@ export const DefaultButtonStyle = styled.button`
   padding: 8px 16px;
   font: ${({ theme }) => theme.fonts.body2};
   border-radius: 10px;
+  cursor: pointer;
 `;
