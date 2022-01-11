bf153e4ef66ae88d64b0ba43e4a6ff87ecf9f958
김진근 <freedom7113@gmail.com>
2022-01-11 13:05:56 +0900

    🍫 (#11) - add input style

--- nutella/src/components/Input/styles.ts
+++ nutella/src/components/Input/styles.ts
@@ -1,3 +1,15 @@
 import styled from "@emotion/styled";
 
-export const InputStyle = styled.input``;
+export const InputStyle = styled.input`
+  border: 0;
+  outline: none;
+  background-color: transparent;
+  font: ${({ theme }) => theme.fonts.body3};
+  color: ${({ theme }) => theme.colors.grayscale.black};
+  padding: 0;
+  margin-bottom: 4px;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.grayscale.gray1};
+  }
+`;
