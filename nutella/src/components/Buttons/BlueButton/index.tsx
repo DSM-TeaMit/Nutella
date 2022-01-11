b9fc086c098fbc4edffa9ff7d9d334b25e1c0e72
김진근 <freedom7113@gmail.com>
2022-01-11 12:38:55 +0900

    🍫 (#11) - default state blue button

--- nutella/src/components/Buttons/BlueButton/index.tsx
+++ nutella/src/components/Buttons/BlueButton/index.tsx
@@ -1,3 +1,8 @@
-const BlueButton = () => {};
+import { FC } from "react";
+import { Button } from "./styles";
+
+const BlueButton: FC = ({ children }) => {
+  return <Button>{children}</Button>;
+};
 
 export default BlueButton;
