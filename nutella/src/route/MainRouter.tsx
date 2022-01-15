009c16057c07e253e32061e6c4f60161c279fbad
김진근 <freedom7113@gmail.com>
2022-01-15 13:14:40 +0900

    🍫 (#22) - use mypage on router

--- nutella/src/route/MainRouter.tsx
+++ nutella/src/route/MainRouter.tsx
@@ -1,11 +1,13 @@
 import { FC, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+import { MypageContainer } from "../container";
 
 const MainRouter: FC = (): JSX.Element => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<div>hello world</div>} />
+        <Route path="/mypage" element={<MypageContainer />} />
       </Routes>
     </Suspense>
   );
