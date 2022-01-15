e96d3881927f34310e1ade54c786b853b390149a
김진근 <freedom7113@gmail.com>
2022-01-15 23:34:38 +0900

    🍫 (#22) - add navs props

--- nutella/src/components/Mypage/Mypage.tsx
+++ nutella/src/components/Mypage/Mypage.tsx
@@ -1,13 +1,41 @@
 import { Route, Routes } from "react-router-dom";
+import NavigationType from "../../interface/Navigation";
 import SideBar from "./SideBar";
 import * as S from "./styles";
+import Personal from "../../assets/icons/personal.svg";
+import Document from "../../assets/icons/document.svg";
+import DocumentEdit from "../../assets/icons/document_edit.svg";
+import Setting from "../../assets/icons/setting.svg";
+
+const navs: NavigationType[] = [
+  {
+    text: "프로필",
+    to: "",
+    icon: Personal,
+  },
+  {
+    text: "프로젝트",
+    to: "project",
+    icon: Document,
+  },
+  {
+    text: "보고서",
+    to: "report",
+    icon: DocumentEdit,
+  },
+  {
+    text: "계정 설정",
+    to: "setting",
+    icon: Setting,
+  },
+];
 
 const MyPage = () => {
   return (
     <S.Container>
       <S.Inner>
         <S.SideBarContainer>
-          <SideBar />
+          <SideBar {...{ navs }} />
         </S.SideBarContainer>
         <S.ContentContainer>
           <Routes>
--- nutella/src/components/Mypage/SideBar/index.tsx
+++ nutella/src/components/Mypage/SideBar/index.tsx
@@ -1,40 +1,13 @@
 import Navigation from "./Navigation";
 import * as S from "./styled";
-import Personal from "../../../assets/icons/personal.svg";
-import Document from "../../../assets/icons/document.svg";
-import DocumentEdit from "../../../assets/icons/document_edit.svg";
-import Setting from "../../../assets/icons/setting.svg";
+import NavigationType from "../../../interface/Navigation";
+import { FC } from "react";
 
-interface NavigationType {
-  to: string;
-  text: string;
-  icon: string;
+interface PropsType {
+  navs: NavigationType[];
 }
 
-const navs: NavigationType[] = [
-  {
-    text: "프로필",
-    to: "",
-    icon: Personal,
-  },
-  {
-    text: "프로젝트",
-    to: "project",
-    icon: Document,
-  },
-  {
-    text: "보고서",
-    to: "report",
-    icon: DocumentEdit,
-  },
-  {
-    text: "계정 설정",
-    to: "setting",
-    icon: Setting,
-  },
-];
-
-const SideBar = () => {
+const SideBar: FC<PropsType> = ({ navs }) => {
   return (
     <S.Container>
       <S.NavContainer>
