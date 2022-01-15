72d39fb95b4e7bd3fbd6a83dfabb52140eeb6166
김진근 <freedom7113@gmail.com>
2022-01-15 22:39:56 +0900

    🍫 (#22) - add navs

--- nutella/src/components/Mypage/SideBar/index.tsx
+++ nutella/src/components/Mypage/SideBar/index.tsx
@@ -1,7 +1,15 @@
+import Navigation from "./Navigation";
 import * as S from "./styled";
 
 const SideBar = () => {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <Navigation to="">profile</Navigation>
+      <Navigation to="project">project</Navigation>
+      <Navigation to="report">report</Navigation>
+      <Navigation to="setting">setting</Navigation>
+    </S.Container>
+  );
 };
 
 export default SideBar;
