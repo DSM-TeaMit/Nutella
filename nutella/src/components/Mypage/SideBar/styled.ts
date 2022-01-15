c3447ba30ab3e5b3983ee384ba971e811076d19a
김진근 <freedom7113@gmail.com>
2022-01-15 23:44:18 +0900

    🍫 (#22) - add style

--- nutella/src/components/Mypage/SideBar/styled.ts
+++ nutella/src/components/Mypage/SideBar/styled.ts
@@ -15,3 +15,34 @@ export const NavContainer = styled.div`
   flex-direction: column;
   row-gap: 8px;
 `;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  column-gap: 12px;
+  align-items: center;
+`;
+
+export const ProfileImage = styled.img`
+  background-color: ${({ theme }) => theme.colors.grayscale.gray1};
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  object-fit: cover;
+  object-position: center;
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 4px;
+`;
+
+export const Name = styled.div`
+  font: ${({ theme }) => theme.fonts.body3};
+  color: ${({ theme }) => theme.colors.grayscale.black};
+`;
+
+export const Email = styled.div`
+  font: ${({ theme }) => theme.fonts.description};
+  color: ${({ theme }) => theme.colors.grayscale.gray2};
+`;
