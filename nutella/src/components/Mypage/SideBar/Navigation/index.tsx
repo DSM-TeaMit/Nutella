281a2974c9026f1bfa118dc4fc10773e030f4481
김진근 <freedom7113@gmail.com>
2022-01-15 22:32:11 +0900

    🍫 (#22) - add navigation container

--- nutella/src/components/Mypage/SideBar/Navigation/index.tsx
+++ nutella/src/components/Mypage/SideBar/Navigation/index.tsx
@@ -1,1 +1,8 @@
-export {};
+import { NavLinkProps } from "react-router-dom";
+import * as S from "./styles";
+
+const Navigation = (props: NavLinkProps) => {
+  return <S.Container {...props} />;
+};
+
+export default Navigation;
--- nutella/src/components/Mypage/SideBar/Navigation/styles.ts
+++ nutella/src/components/Mypage/SideBar/Navigation/styles.ts
@@ -1,1 +1,12 @@
-export {};
+import styled from "@emotion/styled";
+import { NavLink } from "react-router-dom";
+
+export const Container = styled(NavLink)`
+  width: 100%;
+  border: 0;
+  border-radius: 10px;
+  outline: none;
+  color: ${({ theme }) => theme.colors.grayscale.gray1};
+  font: ${({ theme }) => theme.fonts.body2};
+  text-decoration: none;
+`;
