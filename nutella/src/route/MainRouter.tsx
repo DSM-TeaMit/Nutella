738c4551d1318e846bc6017a39f69bce204fa0f0
kky04994 <kkyo8747@naver.com>
2022-01-16 02:14:43 +0900

    🍫 (#3) - change the main screen

--- nutella/src/route/MainRouter.tsx
+++ nutella/src/route/MainRouter.tsx
@@ -1,11 +1,12 @@
 import { FC, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+import Login from "../components/Login/Login";
 
 const MainRouter: FC = (): JSX.Element => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        <Route path="/" element={<div>hello world</div>} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </Suspense>
   );
