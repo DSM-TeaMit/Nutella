3acf2ffdc51f8bc6faf8796ad3404dc25861178e
김진근 <freedom7113@gmail.com>
2022-01-15 23:59:24 +0900

    🍫 (#22) - add contents inner style

--- nutella/src/components/Mypage/styles.ts
+++ nutella/src/components/Mypage/styles.ts
@@ -21,3 +21,12 @@ export const SideBarContainer = styled.div`
 export const ContentContainer = styled.div`
   grid-column: 2 / 5;
 `;
+
+export const ContentInner = styled.div`
+  width: 100%;
+  padding: 20px 32px 60px 32px;
+  background-color: ${({ theme }) => theme.colors.grayscale.white};
+  border: solid 1px ${({ theme }) => theme.colors.grayscale.lightGray1};
+  border-radius: 10px;
+  min-height: 664px;
+`;
