2086080c211b7a166ca70b3d079d5fdd7bd5f9ae
김진근 <freedom7113@gmail.com>
2022-01-15 23:05:16 +0900

    🍫 (#22) - add nav array

--- nutella/src/components/Mypage/SideBar/index.tsx
+++ nutella/src/components/Mypage/SideBar/index.tsx
@@ -1,17 +1,48 @@
 import Navigation from "./Navigation";
 import * as S from "./styled";
 import Personal from "../../../assets/icons/personal.svg";
+import Document from "../../../assets/icons/document.svg";
+import DocumentEdit from "../../../assets/icons/document_edit.svg";
+import Setting from "../../../assets/icons/setting.svg";
+
+interface NavigationType {
+  to: string;
+  text: string;
+  icon: string;
+}
 
 const SideBar = () => {
+  const navs: NavigationType[] = [
+    {
+      text: "프로필",
+      to: "",
+      icon: Personal,
+    },
+    {
+      text: "프로젝트",
+      to: "project",
+      icon: Document,
+    },
+    {
+      text: "보고서",
+      to: "report",
+      icon: DocumentEdit,
+    },
+    {
+      text: "계정 설정",
+      to: "setting",
+      icon: Setting,
+    },
+  ];
+
   return (
     <S.Container>
-      <Navigation to="">
-        <img alt="profile" src={Personal} />
-        <div>프로필</div>
-      </Navigation>
-      <Navigation to="project">project</Navigation>
-      <Navigation to="report">report</Navigation>
-      <Navigation to="setting">setting</Navigation>
+      {navs.map(({ to, text, icon }) => (
+        <Navigation to={to}>
+          <img alt="profile" src={icon} />
+          <div>{text}</div>
+        </Navigation>
+      ))}
     </S.Container>
   );
 };
