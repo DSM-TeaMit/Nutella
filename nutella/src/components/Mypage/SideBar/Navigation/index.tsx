782023038f5aea31ccf0dd4d5a90f23a9e78c210
김진근 <freedom7113@gmail.com>
2022-01-16 20:45:48 +0900

    😎 (#22) - fix navlink error

--- nutella/src/components/Mypage/SideBar/Navigation/index.tsx
+++ nutella/src/components/Mypage/SideBar/Navigation/index.tsx
@@ -1,20 +1,11 @@
-import { useState } from "react";
+import { css } from "@emotion/css";
+import { Theme, ThemeContext } from "@emotion/react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { NavLinkProps } from "react-router-dom";
 import * as S from "./styles";
 
 const Navigation = (props: NavLinkProps) => {
-  const [active, setActive] = useState(false);
-
-  return (
-    <S.Container
-      {...{ active }}
-      style={({ isActive }) => {
-        setActive(isActive);
-        return {};
-      }}
-      {...props}
-    />
-  );
+  return <S.Container className={({ isActive }) => (isActive ? "active" : "")} {...props} />;
 };
 
 export default Navigation;
--- nutella/src/components/Mypage/SideBar/Navigation/styles.ts
+++ nutella/src/components/Mypage/SideBar/Navigation/styles.ts
@@ -1,31 +1,37 @@
 import styled from "@emotion/styled";
 import { NavLink } from "react-router-dom";
 
-interface ContainerProps {
-  active: boolean;
-}
-
-export const Container = styled(NavLink)<ContainerProps>`
+export const Container = styled(NavLink)`
   width: 100%;
   border: 0;
   border-radius: 10px;
   outline: none;
-  color: ${({ theme, active }) =>
-    active ? theme.colors.grayscale.white : theme.colors.grayscale.gray1};
-  background-color: ${({ theme, active }) =>
-    active ? theme.colors.primary.default : theme.colors.grayscale.white};
+  color: ${({ theme }) => theme.colors.grayscale.gray1};
+  background-color: ${({ theme }) => theme.colors.grayscale.white};
   font: ${({ theme }) => theme.fonts.body2};
   text-decoration: none;
   padding: 12px 16px;
   display: flex;
   column-gap: 12px;
 
-  &:hover {
-    background-color: ${({ theme, active }) => !active && theme.colors.grayscale.lightGray1};
+  img {
+    filter: ${({ theme }) => theme.filters.grayscale.gray1};
   }
 
-  img {
-    filter: ${({ theme, active }) =>
-      active ? theme.filters.grayscale.white : theme.filters.grayscale.gray1};
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.grayscale.lightGray1};
+  }
+
+  &.active {
+    color: ${({ theme }) => theme.colors.grayscale.white};
+    background-color: ${({ theme }) => theme.colors.primary.default};
+
+    img {
+      filter: ${({ theme }) => theme.filters.grayscale.white};
+    }
+
+    &:hover {
+      background-color: ${({ theme }) => theme.colors.primary.default};
+    }
   }
 `;
