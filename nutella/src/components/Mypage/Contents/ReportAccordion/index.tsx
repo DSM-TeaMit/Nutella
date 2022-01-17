517bbcc8fcf068f908eb58ef6017fb72bbb778fb
김진근 <freedom7113@gmail.com>
2022-01-18 00:19:11 +0900

    ➕️ (#22) - realize toggle accordion

--- nutella/src/components/Mypage/Contents/ReportAccordion/index.tsx
+++ nutella/src/components/Mypage/Contents/ReportAccordion/index.tsx
@@ -1,17 +1,36 @@
 import * as S from "./styles";
 import Arrow from "../../../../assets/icons/up_arrow.svg";
 import ReportCard from "../../../ReportCard";
+import { useEffect, useRef, useState } from "react";
 
 const ReportAccordion = () => {
+  const [isActive, setIsActive] = useState(false);
+  const container = useRef<HTMLDivElement>(null);
+  const header = useRef<HTMLDivElement>(null);
+
+  const padding = 12;
+
+  useEffect(() => {
+    if (container.current && header.current) {
+      if (isActive) {
+        container.current.style.height = "unset";
+      } else {
+        container.current.style.height = `${header.current.offsetHeight + padding * 2}px`;
+      }
+    }
+  }, [isActive]);
+
   return (
-    <S.Container>
-      <S.HeaderContainer>
-        <div>
-          <S.Title>승인 대기중인 보고서&nbsp;</S.Title>
-          <S.Count>12</S.Count>
-        </div>
-        <img alt="arrow" src={Arrow} />
-      </S.HeaderContainer>
+    <S.Container ref={container}>
+      <div ref={header}>
+        <S.HeaderContainer onClick={() => setIsActive(!isActive)}>
+          <div>
+            <S.Title>승인 대기중인 보고서&nbsp;</S.Title>
+            <S.Count>12</S.Count>
+          </div>
+          <img alt="arrow" src={Arrow} />
+        </S.HeaderContainer>
+      </div>
       <S.ContentContainer>
         <ReportCard />
         <ReportCard />
--- nutella/src/components/Mypage/Contents/ReportAccordion/styles.ts
+++ nutella/src/components/Mypage/Contents/ReportAccordion/styles.ts
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 export const Container = styled.div`
   width: 100%;
   padding: 12px 0px;
+  overflow: hidden;
 `;
 
 export const HeaderContainer = styled.button`
