f672d9fe6bbaad374b70f8546c79534760601403
김진근 <freedom7113@gmail.com>
2022-01-18 00:07:32 +0900

    🍫 (#22) - add accordion style

--- nutella/src/components/Mypage/Contents/ReportAccordion/index.tsx
+++ nutella/src/components/Mypage/Contents/ReportAccordion/index.tsx
@@ -1,6 +1,26 @@
+import * as S from "./styles";
+import Arrow from "../../../../assets/icons/up_arrow.svg";
+import ReportCard from "../../../ReportCard";
 
 const ReportAccordion = () => {
-
-}
+  return (
+    <S.Container>
+      <S.HeaderContainer>
+        <div>
+          <S.Title>승인 대기중인 보고서</S.Title>
+          <S.Count>12</S.Count>
+        </div>
+        <img alt="arrow" src={Arrow} />
+      </S.HeaderContainer>
+      <S.ContentContainer>
+        <ReportCard />
+        <ReportCard />
+        <ReportCard />
+        <ReportCard />
+        <ReportCard />
+      </S.ContentContainer>
+    </S.Container>
+  );
+};
 
 export default ReportAccordion;
--- nutella/src/components/Mypage/Contents/ReportAccordion/styles.ts
+++ nutella/src/components/Mypage/Contents/ReportAccordion/styles.ts
@@ -1,1 +1,33 @@
-export {};
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  width: 100%;
+  padding: 12px 0px;
+`;
+
+export const HeaderContainer = styled.button`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 0;
+  outline: none;
+  cursor: pointer;
+`;
+
+export const Title = styled.span`
+  font: ${({ theme }) => theme.fonts.h3};
+  color: ${({ theme }) => theme.colors.grayscale.black};
+`;
+
+export const Count = styled.span`
+  font: ${({ theme }) => theme.fonts.h3};
+  color: ${({ theme }) => theme.colors.grayscale.black};
+`;
+
+export const ContentContainer = styled.div`
+  margin-top: 16px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 20px;
+  row-gap: 20px;
+`;
