9c4e39b2ab2008d36d301da902ea94852c4e7b41
kky04994 <kkyo8747@naver.com>
2022-01-19 04:08:15 +0900

    🍫 (#28) - adding searchcontainer on the router

--- nutella/src/route/MainRouter.tsx
+++ nutella/src/route/MainRouter.tsx
@@ -1,11 +1,13 @@
 import { FC, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+import { SearchContainer } from "../container";
 
 const MainRouter: FC = (): JSX.Element => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<div>hello world</div>} />
+        <Route path="/search" element={<SearchContainer />} />
       </Routes>
     </Suspense>
   );
