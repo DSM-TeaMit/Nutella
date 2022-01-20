d32a677fa57a13588c1009156058cd8058159803
김진근 <freedom7113@gmail.com>
2022-01-20 17:54:10 +0900

    🍫 (#35) - use modal context in modal controller

--- nutella/src/components/ModalController/index.tsx
+++ nutella/src/components/ModalController/index.tsx
@@ -1,15 +1,16 @@
-import TestModal from "../TestModal";
 import * as S from "./styles";
 import ReactDOM from "react-dom";
+import useModalContext from "../../hooks/useModalContext";
 
 const ModalController = () => {
+  const { modals } = useModalContext();
+
   const el = document.getElementById("modal")!;
 
-  return ReactDOM.createPortal(
-    <S.Background>
-      <TestModal />
-    </S.Background>,
-    el
+  return modals.length > 0 ? (
+    ReactDOM.createPortal(<S.Background>{modals[modals.length - 1]}</S.Background>, el)
+  ) : (
+    <></>
   );
 };
 
