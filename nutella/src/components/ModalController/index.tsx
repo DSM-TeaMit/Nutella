3be965286b81f436a781e2e3750d659465f77bf2
김진근 <freedom7113@gmail.com>
2022-01-20 18:09:40 +0900

    🍫 (#35) - add click event lister on window

--- nutella/src/components/ModalController/index.tsx
+++ nutella/src/components/ModalController/index.tsx
@@ -1,19 +1,33 @@
 import * as S from "./styles";
 import ReactDOM from "react-dom";
 import useModalContext from "../../hooks/useModalContext";
+import { useCallback, useEffect } from "react";
 
 const ModalController = () => {
   const { modals, closeCurrentModal } = useModalContext();
 
   const el = document.getElementById("modal")!;
 
-  const onOutsideClick = () => {
-    closeCurrentModal();
-  };
+  const onOutsideClick = useCallback(
+    (e: MouseEvent) => {
+      closeCurrentModal();
+    },
+    [closeCurrentModal]
+  );
+
+  useEffect(() => {
+    if (modals.length > 0) {
+      window.addEventListener("click", onOutsideClick);
+    } else {
+      window.removeEventListener("click", onOutsideClick);
+    }
+  }, [modals, onOutsideClick]);
 
   return modals.length > 0 ? (
     ReactDOM.createPortal(
-      <S.Background onClick={onOutsideClick}>{modals[modals.length - 1]}</S.Background>,
+      <S.Background>
+        <div>{modals[modals.length - 1]}</div>
+      </S.Background>,
       el
     )
   ) : (
