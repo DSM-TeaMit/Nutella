e059c02fae4c1eae74eb3233ec5df1d998068dc8
김진근 <freedom7113@gmail.com>
2022-01-20 20:25:06 +0900

    ➕️ (#38) - add message provider

---
+++ nutella/src/components/Providers/MessageProvider.tsx
@@ -0,0 +1,7 @@
+import { MessageContext } from "../../context/MessageContext";
+
+const MessageProvider = () => {
+  return <MessageContext.Provider></MessageContext.Provider>;
+};
+
+export default MessageProvider;
