6a66d0ef7a2efad514d2ad040021c70a9502223b
김진근 <freedom7113@gmail.com>
2022-01-20 21:10:19 +0900

    🍫 (#38) - add render message function

--- nutella/src/components/MessageController/index.tsx
+++ nutella/src/components/MessageController/index.tsx
@@ -1,8 +1,30 @@
+import React, { FC, useMemo } from "react";
 import ReactDOM from "react-dom";
+import { MessageType } from "../../context/MessageContext";
+import useMessageContext from "../../hooks/useMessageContext";
+import DenialMessage from "../Message/DenialMessage";
+import PositiveMessage from "../Message/PositiveMessage";
 import * as S from "./styles";
 
 const MessageController = () => {
+  const { messages } = useMessageContext();
+
   const el = document.getElementById("message")!;
+
+  const renderMessages = useMemo(
+    () =>
+      messages.map((value) => {
+        const { type } = value;
+        const componentMap = new Map<string, FC<MessageType>>()
+          .set("Positive", PositiveMessage)
+          .set("Denial", DenialMessage);
+
+        const component = componentMap.get(type)!;
+
+        return React.createElement(component, value);
+      }),
+    [messages]
+  );
 
   return ReactDOM.createPortal(
     <S.Container>
