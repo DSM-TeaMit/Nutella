097e88b93dedb2324f6ec419ce838745204e5c7c
김진근 <freedom7113@gmail.com>
2022-01-20 17:42:20 +0900

    ➕️ (#35) - add ModalController

---
+++ nutella/src/components/ModalController/index.tsx
@@ -0,0 +1,7 @@
+import * as S from "./styles";
+
+const ModalController = () => {
+  return <S.Background></S.Background>;
+};
+
+export default ModalController;
---
+++ nutella/src/components/ModalController/styles.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const Background = styled.div`
+  background-color: ${({ theme }) => theme.colors.grayscale.black}50;
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+`;
