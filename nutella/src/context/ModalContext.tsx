b9eeaef1b8a12eed216fb5d0c97d292ea854a33b
김진근 <freedom7113@gmail.com>
2022-01-20 20:11:17 +0900

    💣 (#39) - remove modal provider in modal context

--- nutella/src/context/ModalContext.tsx
+++ nutella/src/context/ModalContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, FC, useCallback, useMemo, useState } from "react";
+import { createContext } from "react";
 
 export interface ModalContentType {
   modals: JSX.Element[];
@@ -13,49 +13,3 @@ export const ModalContext = createContext<ModalContentType>({
   closeByStep: () => {},
   openModal: () => {},
 });
-
-export const ModalProvider: FC = ({ children }) => {
-  const [modals, setModals] = useState<JSX.Element[]>([]);
-
-  const closeCurrentModal = useCallback(() => {
-    if (modals.length <= 0) {
-      return;
-    }
-
-    const copyModals = [...modals];
-    copyModals.pop();
-
-    setModals(copyModals);
-  }, [modals]);
-
-  const closeByStep = useCallback((step: number) => {
-    if (step <= 0 || !Number.isInteger(step)) {
-      throw new Error(
-        "ModalContext의 closeByStep의 매개변수로 오는 step은 양의 정수이여야 합니다."
-      );
-    }
-
-    const fixedStep = step > modals.length ? modals.length : step;
-    const copyModals = [...modals];
-
-    new Array(fixedStep).fill(0).forEach(() => {
-      copyModals.pop();
-    });
-
-    setModals(copyModals);
-  }, []);
-
-  const openModal = useCallback(
-    (modal: JSX.Element) => {
-      setModals([...modals, modal]);
-    },
-    [modals]
-  );
-
-  const value = useMemo<ModalContentType>(
-    () => ({ modals, closeCurrentModal, openModal, closeByStep }),
-    [closeByStep, closeCurrentModal, modals, openModal]
-  );
-
-  return <ModalContext.Provider value={value}>{children}</ModalContext.Provider>;
-};
