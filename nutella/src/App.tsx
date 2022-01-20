61fc675d47d468dd6f144e13b8365537930a96cb
김진근 <freedom7113@gmail.com>
2022-01-20 17:35:37 +0900

    🍫 (#35) - use modalProvider

--- nutella/src/App.tsx
+++ nutella/src/App.tsx
@@ -1,4 +1,5 @@
 import { Global, ThemeProvider } from "@emotion/react";
+import { ModalProvider } from "./context/ModalContext";
 import RootRouter from "./route";
 import { reset } from "./style/globalStyle";
 import theme from "./utils/theme/theme";
@@ -6,8 +7,10 @@ import theme from "./utils/theme/theme";
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Global styles={reset} />
-      <RootRouter />
+      <ModalProvider>
+        <Global styles={reset} />
+        <RootRouter />
+      </ModalProvider>
     </ThemeProvider>
   );
 }
