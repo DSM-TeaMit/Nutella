d499e374041cdf4c55d44993bb43ec0910023be0
김진근 <freedom7113@gmail.com>
2022-01-21 16:04:59 +0900

    🍫 (#43) - on project add button click then modal up

--- nutella/src/components/Mypage/Contents/Project.tsx
+++ nutella/src/components/Mypage/Contents/Project.tsx
@@ -1,8 +1,21 @@
 import * as S from "./styles";
 import { ContentInner } from "../styles";
 import ProjectCard from "../../ProjectCard";
+import { useCallback } from "react";
+import useModalContext from "../../../hooks/useModalContext";
+import ProjectAddModal from "../../Modals/ProejctAddModal";
 
 const Project = () => {
+  const { openModal } = useModalContext();
+
+  const onMessageAddClick = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation();
+      openModal(<ProjectAddModal />);
+    },
+    [openModal]
+  );
+
   return (
     <ContentInner>
       <S.Container>
@@ -12,7 +25,7 @@ const Project = () => {
               <S.H3>프로젝트&nbsp;</S.H3>
               <S.BlueH3>12</S.BlueH3>
             </div>
-            <S.AddProject>+ 프로젝트 추가</S.AddProject>
+            <S.AddProject onClick={onMessageAddClick}>+ 프로젝트 생성</S.AddProject>
           </S.ProjectTitle>
           <S.Grid>
             {new Array(12).fill(0).map((_, index) => (
