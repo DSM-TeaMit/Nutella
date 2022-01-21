7a58969d1c0ab60cff31d5fb1e310d5e56dea287
김진근 <freedom7113@gmail.com>
2022-01-21 23:44:06 +0900

    🍫 (#43) - add content

--- nutella/src/components/Modals/ProejctAddModal/index.tsx
+++ nutella/src/components/Modals/ProejctAddModal/index.tsx
@@ -1,7 +1,30 @@
+import useInput from "../../../hooks/useInput";
+import Input from "../../Input";
+import TagInput from "../../TagInput";
 import * as S from "./styles";
 
 const ProjectAddModal = () => {
-  return <S.Container></S.Container>;
+  const [inputProps] = useInput("", true);
+
+  return (
+    <S.Container>
+      <S.Inner>
+        <S.Title>프로젝트 생성</S.Title>
+        <S.ContentContainer>
+          <S.Subtitle>프로젝트 이름</S.Subtitle>
+          <Input placeholder="프로젝트 이름을 입력해주세요..." />
+        </S.ContentContainer>
+        <S.ContentContainer>
+          <S.Subtitle>프로젝트 분야</S.Subtitle>
+          <TagInput placeholder="프로젝트 분야를 입력해주세요..." {...inputProps} />
+        </S.ContentContainer>
+        <S.ContentContainer>
+          <S.Subtitle>멤버</S.Subtitle>
+          <Input placeholder="학번 또는 이름으로 유저 검색..." />
+        </S.ContentContainer>
+      </S.Inner>
+    </S.Container>
+  );
 };
 
 export default ProjectAddModal;
--- nutella/src/components/Modals/ProejctAddModal/styles.ts
+++ nutella/src/components/Modals/ProejctAddModal/styles.ts
@@ -7,6 +7,13 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   row-gap: 36px;
+  min-width: 550px;
+`;
+
+export const Inner = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 40px;
 `;
 
 export const Title = styled.div`
@@ -14,4 +21,14 @@ export const Title = styled.div`
   color: ${({ theme }) => theme.colors.grayscale.black};
 `;
 
-export const;
+export const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 12px;
+  width: 100%;
+`;
+
+export const Subtitle = styled.div`
+  font: ${({ theme }) => theme.fonts.body3};
+  color: ${({ theme }) => theme.colors.grayscale.gray2};
+`;
