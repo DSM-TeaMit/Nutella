f1bc8c827466286649b51ef76d1cc47d8d1e76ee
김진근 <freedom7113@gmail.com>
2022-01-22 21:35:09 +0900

    🍫 (#47) - realize on input function

--- nutella/src/components/MarkdownEditor/Row.tsx
+++ nutella/src/components/MarkdownEditor/Row.tsx
@@ -1,35 +1,42 @@
-import { FC, useEffect, useRef, useState } from "react";
-import { Row as RowType } from "../../context/MarkdownCotext";
+import { FC, useContext, useEffect, useRef, useState } from "react";
+import { MarkdownContext, Row as RowType } from "../../context/MarkdownCotext";
 
 interface PropsType {
   data: RowType;
 }
 
 const Row: FC<PropsType> = ({ data }) => {
+  const { text, id } = data;
+  const { changeText, addRowAfterId, removeRowById } = useContext(MarkdownContext);
+
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    ref.current?.focus();
+    if (ref.current) {
+      ref.current.focus();
+      ref.current.innerText = text;
+    }
   }, []);
 
   const onKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
     if (e.key === "Enter") {
       e.stopPropagation();
       e.preventDefault();
+      addRowAfterId(id);
     }
 
-    if (e.key === "Backspace" && value === "") {
+    if (e.key === "Backspace" && text === "") {
+      removeRowById(id);
     }
   };
 
-  return (
-    <div
-      ref={ref}
-      onKeyDown={onKeyDown}
-      contentEditable
-      onInput={(e) => setValue((e.target as Node).textContent || "")}
-    />
-  );
+  const onInput = (e: React.FormEvent<HTMLDivElement>) => {
+    console.log((e.target as Node).textContent);
+
+    changeText(id, (e.target as Node).textContent || "");
+  };
+
+  return <div ref={ref} onKeyDown={onKeyDown} contentEditable onInput={onInput} />;
 };
 
 export default Row;
