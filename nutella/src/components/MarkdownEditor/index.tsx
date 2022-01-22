9dd924f01cb9896e74131f610e19a6db907bad10
김진근 <freedom7113@gmail.com>
2022-01-22 16:16:39 +0900

    🍫 (#47) - add markdown editor component

---
+++ nutella/src/components/MarkdownEditor/index.tsx
@@ -0,0 +1,3 @@
+const MarkdownEditor = () => {};
+
+export default MarkdownEditor;
---
+++ nutella/src/components/MarkdownEditor/styles.ts
@@ -0,0 +1,4 @@
+import styled from "@emotion/styled";
+import markdownStyle from "../../constant/MarkdownStyle";
+
+export const Container = styled(markdownStyle)``;
