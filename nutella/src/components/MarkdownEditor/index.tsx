34cceac7ea7178f31ce12e8c80b26fb9cafa1f9b
김진근 <freedom7113@gmail.com>
2022-01-22 19:46:15 +0900

    🍫 (#47) - make markdown editor

--- nutella/src/components/MarkdownEditor/index.tsx
+++ nutella/src/components/MarkdownEditor/index.tsx
@@ -1,3 +1,17 @@
-const MarkdownEditor = () => {};
+import { useState } from "react";
+import Row from "./Row";
+import * as S from "./styles";
+
+const MarkdownEditor = () => {
+  const [count, setCount] = useState(1);
+
+  return (
+    <S.Container>
+      {new Array(count).fill(0).map(() => (
+        <Row setCount={setCount} count={count} />
+      ))}
+    </S.Container>
+  );
+};
 
 export default MarkdownEditor;
--- nutella/src/components/MarkdownEditor/styles.ts
+++ nutella/src/components/MarkdownEditor/styles.ts
@@ -1,4 +1,7 @@
 import styled from "@emotion/styled";
 import markdownStyle from "../../constant/MarkdownStyle";
 
-export const Container = styled(markdownStyle)``;
+export const Container = styled(markdownStyle)`
+  width: 100%;
+  height: 100%;
+`;
