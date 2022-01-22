fa5feba19d991e395c94b3f792f33bfd20dd215f
김진근 <freedom7113@gmail.com>
2022-01-22 12:13:03 +0900

    ➕️ (#43) - add useTagInput hook

---
+++ nutella/src/hooks/useTagInput.ts
@@ -0,0 +1,37 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { Tag } from "../components/TagInput";
+import State from "../interface/State";
+
+type Value = string | number;
+
+interface InputProps {
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value: Value;
+  clearValue: () => void;
+  tagState: State<Tag[]>;
+}
+
+type Dispatch = [InputProps, State<Value>];
+
+const useTagInput = (initValue?: Value, debug?: boolean): Dispatch => {
+  const [value, setValue] = useState<Value>(initValue || "");
+  const [tags, setTags] = useState<Tag[]>([]);
+
+  const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  }, []);
+
+  useEffect(() => {
+    if (debug) {
+      console.log(tags);
+    }
+  }, [debug, tags]);
+
+  const clearValue = useCallback(() => {
+    setValue("");
+  }, []);
+
+  return [{ onChange, value, clearValue, tagState: [tags, setTags] }, [value, setValue]];
+};
+
+export default useTagInput;
