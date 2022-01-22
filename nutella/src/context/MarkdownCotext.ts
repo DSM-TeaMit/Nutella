9a894f24c1c09b06c41b4d0089a106de6eea027d
김진근 <freedom7113@gmail.com>
2022-01-22 19:58:34 +0900

    ➕️ (#47) - add markdown context

---
+++ nutella/src/context/MarkdownCotext.ts
@@ -0,0 +1,21 @@
+import { createContext } from "react";
+
+export interface Row {
+  id: string;
+  text: string;
+  type: string;
+}
+
+export interface MarkdownContextType {
+  rows: Row[];
+  addRowAfterId: (id: string) => void;
+  removeRowById: (id: string) => void;
+  changeRowType: (id: string) => void;
+}
+
+export const MarkdownContext = createContext<MarkdownContextType>({
+  rows: [],
+  addRowAfterId: () => {},
+  changeRowType: () => {},
+  removeRowById: () => {},
+});
