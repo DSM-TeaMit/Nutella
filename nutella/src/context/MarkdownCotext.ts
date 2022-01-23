46ed476a745735ab3b001634a87eed551ac2ace3
김진근 <freedom7113@gmail.com>
2022-01-23 12:52:00 +0900

    🍫 (#47) - add change vertival focus function in context

--- nutella/src/context/MarkdownCotext.ts
+++ nutella/src/context/MarkdownCotext.ts
@@ -8,18 +8,20 @@ export interface Row {
 
 export interface MarkdownContextType {
   rows: Row[];
+  refs: React.RefObject<HTMLDivElement[]>;
   addRowAfterId: (id: string) => void;
   removeRowById: (id: string) => void;
   changeRowType: (id: string, type: string) => void;
   changeText: (id: string, text: string) => void;
-  refs: React.RefObject<HTMLDivElement[]>;
+  changeVerticalFocus: (id: string, step: number) => void;
 }
 
 export const MarkdownContext = createContext<MarkdownContextType>({
   rows: [],
+  refs: createRef<HTMLDivElement[]>(),
   addRowAfterId: () => {},
   changeRowType: () => {},
   removeRowById: () => {},
   changeText: () => {},
-  refs: createRef<HTMLDivElement[]>(),
+  changeVerticalFocus: () => {},
 });
