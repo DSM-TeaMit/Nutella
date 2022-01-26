8ba936462f843bd8df58e7cd0decf06b5a1f4f89
김진근 <freedom7113@gmail.com>
2022-01-26 21:30:24 +0900

    🍫 (#53) - add project in route

--- nutella/src/route/MainRouter.tsx
+++ nutella/src/route/MainRouter.tsx
@@ -6,9 +6,16 @@ const MainRouter: FC = (): JSX.Element => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        <Route path="/" element={<LoginContainer />} />
-        <Route path="/search" element={<SearchContainer />} />
-        <Route path="/mypage/*" element={<MypageContainer />} />
+        <Route path="/*">
+          <Route path="" element={<LoginContainer />} />
+          <Route path="search" element={<SearchContainer />} />
+          <Route path="mypage/*" element={<MypageContainer />} />
+          <Route path="project/:id/*">
+            <Route path="" element={<div>this is project</div>} />
+            <Route path="plan" element={<div>this is project plan</div>} />
+            <Route path="result" element={<div>this is project result</div>} />
+          </Route>
+        </Route>
       </Routes>
     </Suspense>
   );
