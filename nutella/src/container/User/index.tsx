ff865a0881ddd8b2b82867d37d0738a8f6780278
김진근 <freedom7113@gmail.com>
2022-01-26 15:12:54 +0900

    🍫 (#51) - add user container

---
+++ nutella/src/container/User/index.tsx
@@ -0,0 +1,15 @@
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import User from "../../components/Profile/User";
+
+const UserContainer = () => {
+  return (
+    <>
+      <Header />
+      <User />
+      <Footer />
+    </>
+  );
+};
+
+export default UserContainer;
