15ecd7bd284a657c3ed8b46b8251c0b041f713d8
김진근 <freedom7113@gmail.com>
2022-01-27 22:22:31 +0900

    🍫 (#53) - add day of week in date picker

--- nutella/src/components/Modals/DatePicker/index.tsx
+++ nutella/src/components/Modals/DatePicker/index.tsx
@@ -1,3 +1,28 @@
-export const DatePicker = () => {};
+import { Theme, ThemeContext } from "@emotion/react";
+import { useContext } from "react";
+import * as S from "./styles";
+
+export const DatePicker = () => {
+  const themeContext = useContext(ThemeContext) as Theme;
+
+  return (
+    <S.Container>
+      <S.Title>날짜를 선택해주세요</S.Title>
+      <S.Date>시작 ~ 종료</S.Date>
+      <S.DateContainer>
+        <S.DateTitle>2022년 1월</S.DateTitle>
+        <S.DOWContainer>
+          <S.DOWCell color={themeContext.colors.red.default}>일</S.DOWCell>
+          <S.DOWCell color={themeContext.colors.grayscale.black}>월</S.DOWCell>
+          <S.DOWCell color={themeContext.colors.grayscale.black}>화</S.DOWCell>
+          <S.DOWCell color={themeContext.colors.grayscale.black}>수</S.DOWCell>
+          <S.DOWCell color={themeContext.colors.grayscale.black}>목</S.DOWCell>
+          <S.DOWCell color={themeContext.colors.grayscale.black}>금</S.DOWCell>
+          <S.DOWCell color={themeContext.colors.primary.default}>토</S.DOWCell>
+        </S.DOWContainer>
+      </S.DateContainer>
+    </S.Container>
+  );
+};
 
 export default DatePicker;
--- nutella/src/components/Modals/DatePicker/styles.ts
+++ nutella/src/components/Modals/DatePicker/styles.ts
@@ -1,1 +1,46 @@
-export {};
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  padding: 16px;
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.colors.grayscale.white};
+`;
+
+export const Title = styled.div`
+  font: ${({ theme }) => theme.fonts.subtitle1};
+  color: ${({ theme }) => theme.colors.grayscale.black};
+  margin-bottom: 8px;
+`;
+
+export const Date = styled.div`
+  font: ${({ theme }) => theme.fonts.body3};
+  color: ${({ theme }) => theme.colors.grayscale.gray2};
+  margin-bottom: 24px;
+`;
+
+export const DateContainer = styled.div`
+  border-radius: 10px;
+  border: 1px solid ${({ theme }) => theme.colors.grayscale.lightGray1};
+  font: ${({ theme }) => theme.fonts.body3};
+`;
+
+export const DateTitle = styled.div`
+  font: ${({ theme }) => theme.fonts.body3};
+  color: ${({ theme }) => theme.colors.grayscale.black};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.grayscale.lightGray1};
+  padding: 6px 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const DOWContainer = styled.div`
+  padding: 4px 0px;
+  display: flex;
+`;
+
+export const DOWCell = styled.div`
+  color: ${({ color }) => color};
+  text-align: center;
+  width: 50px;
+`;
