e8e624adfef816cabe4dfa2638dea1b26266e679
김진근 <freedom7113@gmail.com>
2022-01-27 19:37:49 +0900

    🍫 (#53) - checkbox ui

--- nutella/src/components/CheckBox/index.tsx
+++ nutella/src/components/CheckBox/index.tsx
@@ -1,3 +1,12 @@
-const CheckBox = () => {};
+import * as S from "./styles";
+
+const CheckBox = () => {
+  return (
+    <S.Contianer>
+      <S.Box isActive={false} />
+      <S.Label></S.Label>
+    </S.Contianer>
+  );
+};
 
 export default CheckBox;
