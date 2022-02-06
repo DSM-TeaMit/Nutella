28111d82a9744bc591bb6d00b8b8d5cd641ff91b
kky04994 <kkyo8747@naver.com>
2022-02-07 05:23:13 +0900

    🍫 (#26) - add signupcontainer

--- nutella/src/route/MainRouter.tsx
+++ nutella/src/route/MainRouter.tsx
@@ -1,15 +1,24 @@
 import { FC, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
-import { SearchContainer, LoginContainer, MypageContainer, UserContainer } from "../container";
+import {
+  SearchContainer,
+  LoginContainer,
+  MypageContainer,
+  UserContainer,
+  SignupContainer,
+} from "../container";
 
 const MainRouter: FC = (): JSX.Element => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        <Route path="/" element={<LoginContainer />} />
-        <Route path="/search" element={<SearchContainer />} />
-        <Route path="/mypage/*" element={<MypageContainer />} />
-        <Route path="/user/:id/*" element={<UserContainer />} />
+        <Route path="/*">
+          <Route path="" element={<LoginContainer />} />
+          <Route path="signup" element={<SignupContainer />} />
+          <Route path="search" element={<SearchContainer />} />
+          <Route path="mypage/*" element={<MypageContainer />} />
+          <Route path="user/:id/*" element={<UserContainer />} />
+        </Route>
       </Routes>
     </Suspense>
   );
