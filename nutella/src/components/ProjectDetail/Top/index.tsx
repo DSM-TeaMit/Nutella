16cf2f5f3081c605355d4c4afb4aef7d413582c7
kky04994 <kkyo8747@naver.com>
2022-02-11 23:27:24 +0900

    ➕(#58) add top file

---
+++ nutella/src/components/ProjectDetail/Top/index.tsx
@@ -0,0 +1,44 @@
+import * as S from "./styles";
+import { ProfileIcons, ViewIcons } from "../../../assets/icons";
+
+const Top = () => {
+  const Field = ["웹", "보안", "임베디드", "대마고"];
+  return (
+    <>
+      <S.TopContainer>
+        <S.TopContent>
+          <S.ProjectImg alt="" src="" />
+          <div>
+            <S.ProjectTop>
+              <S.ProjectName>Teamit</S.ProjectName>
+              <S.ProjectRincian>
+                <div>
+                  <img src={ViewIcons} />
+                  <S.Font>123</S.Font>
+                  <img src={ProfileIcons} />
+                  <S.Font>팀 프로젝트</S.Font>
+                </div>
+                <S.Modify>수정</S.Modify>
+              </S.ProjectRincian>
+            </S.ProjectTop>
+            <S.ProjectContent>
+              사면·감형 및 복권에 관한 사항은 법률로 정한다. 모든 국민은 주거의
+              자유를 침해받지 아니한다. 주거에 대한 압수나 수색을 할 때에는
+              검사의 신청에 의하여 법관이 발부한 영장을 제시하여야 한다.
+            </S.ProjectContent>
+            <S.ProjectBottom>
+              <div>
+                {Field.map((data, index) => {
+                  return <S.Field key={index}>{data}</S.Field>;
+                })}
+              </div>
+              <S.Step>계획중...</S.Step>
+            </S.ProjectBottom>
+          </div>
+        </S.TopContent>
+      </S.TopContainer>
+    </>
+  );
+};
+
+export default Top;
