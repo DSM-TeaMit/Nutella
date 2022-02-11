69d25cdf2b2ff0e9262b2f1cfabb99db0cf203c4
kky04994 <kkyo8747@naver.com>
2022-02-12 02:35:11 +0900

    ➕(#58) add projectdetailcontainer file

---
+++ nutella/src/container/ProjectDetail/index.tsx
@@ -0,0 +1,15 @@
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import ProjectDetail from "../../components/ProjectDetail/ProjectDetail";
+
+const ProjectDetailContainer = () => {
+  return (
+    <>
+      <Header />
+      <ProjectDetail />
+      <Footer />
+    </>
+  );
+};
+
+export default ProjectDetailContainer;
