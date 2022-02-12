82701cc1aaa7573670bcb04505a4d95942055c7d
kky04994 <kkyo8747@naver.com>
2022-02-12 20:48:48 +0900

    ➕(#27) add feedcontainer file

---
+++ nutella/src/container/Feed/index.tsx
@@ -0,0 +1,15 @@
+import Header from "../../components/Header/Header";
+import Feed from "../../components/Feed/Feed";
+import Footer from "../../components/Footer/Footer";
+
+const FeedContainer = () => {
+  return (
+    <>
+      <Header />
+      <Feed />
+      <Footer />
+    </>
+  );
+};
+
+export default FeedContainer;
