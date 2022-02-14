2df0992cb332c7884506c61866a66ca6d8e0c8a2
KJG04 <freedom7113@gmail.com>
2022-02-15 01:54:21 +0900

    🍫 (#62) - add container in result

--- nutella/src/components/Result/Personal.tsx
+++ nutella/src/components/Result/Personal.tsx
@@ -1,1 +1,13 @@
-export {};
+import * as S from "./styles";
+
+const Personal = () => {
+  return (
+    <S.Container>
+      <S.ContentContainer>
+        <S.ContentInner></S.ContentInner>
+      </S.ContentContainer>
+    </S.Container>
+  );
+};
+
+export default Personal;
--- nutella/src/components/Result/styles.ts
+++ nutella/src/components/Result/styles.ts
@@ -1,1 +1,25 @@
-export {};
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  display: flex;
+  row-gap: 40px;
+  flex-direction: column;
+  width: 920px;
+  margin: 0px auto;
+  margin-top: 120px;
+  margin-bottom: 200px;
+`;
+
+export const ContentContainer = styled.div`
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.grayscale.white};
+  padding: 40px;
+  border-radius: 10px;
+  border: 1px solid ${({ theme }) => theme.colors.grayscale.lightGray1};
+`;
+
+export const ContentInner = styled.div`
+  width: 100%;
+  border: 1px solid ${({ theme }) => theme.colors.grayscale.black};
+  border-radius: 10px;
+`;
