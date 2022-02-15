964713b9519792005c3b753937c019c0598f7ce9
KJG04 <freedom7113@gmail.com>
2022-02-15 10:55:54 +0900

    🍫 (#62) - add cover in result

--- nutella/src/components/Result/Personal.tsx
+++ nutella/src/components/Result/Personal.tsx
@@ -4,7 +4,25 @@ const Personal = () => {
   return (
     <S.Container>
       <S.ContentContainer>
-        <S.ContentInner></S.ContentInner>
+        <S.ContentInner>
+          <S.CoverInner>
+            <div>
+              <S.CoverBig>개인 프로젝트 보고서</S.CoverBig>
+              <S.Topic placeholder="주제" />
+              <S.CoverInfoContainer>
+                <S.CoverInfoRow>
+                  <S.CoverInfoTitle>제출자</S.CoverInfoTitle>
+                  <S.CoverInfoContent>2학년 1반 5번 김진근</S.CoverInfoContent>
+                </S.CoverInfoRow>
+                <S.CoverInfoRow>
+                  <S.CoverInfoTitle>담당 교사 확인</S.CoverInfoTitle>
+                  <S.CoverInfoContent />
+                </S.CoverInfoRow>
+              </S.CoverInfoContainer>
+            </div>
+            <S.CoverBig>대덕소프트웨어마이스터고등학교</S.CoverBig>
+          </S.CoverInner>
+        </S.ContentInner>
       </S.ContentContainer>
     </S.Container>
   );
--- nutella/src/components/Result/styles.ts
+++ nutella/src/components/Result/styles.ts
@@ -16,10 +16,72 @@ export const ContentContainer = styled.div`
   padding: 40px;
   border-radius: 10px;
   border: 1px solid ${({ theme }) => theme.colors.grayscale.lightGray1};
+  aspect-ratio: 1 / 1.414;
 `;
 
 export const ContentInner = styled.div`
   width: 100%;
+  height: 100%;
   border: 1px solid ${({ theme }) => theme.colors.grayscale.black};
   border-radius: 10px;
+`;
+
+export const CoverInner = styled.div`
+  padding: 72px 36px;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const CoverBig = styled.div`
+  font: ${({ theme }) => theme.fonts.h1};
+  color: ${({ theme }) => theme.colors.grayscale.black};
+  text-align: center;
+`;
+
+export const Topic = styled.input`
+  font: ${({ theme }) => theme.fonts.h2};
+  color: ${({ theme }) => theme.colors.grayscale.black};
+  width: 100%;
+  padding: 34px 0px;
+  text-align: center;
+  margin-top: 100px;
+  margin-bottom: 150px;
+  border: 1px solid ${({ theme }) => theme.colors.grayscale.black};
+  border-radius: 10px;
+  outline: none;
+
+  ::placeholder {
+    color: ${({ theme }) => theme.colors.grayscale.gray1};
+  }
+`;
+
+export const CoverInfoContainer = styled.div`
+  border: 1px solid ${({ theme }) => theme.colors.grayscale.black};
+  border-radius: 10px;
+  font: ${({ theme }) => theme.fonts.subtitle1};
+`;
+
+export const CoverInfoRow = styled.div`
+  border-bottom: 1px solid ${({ theme }) => theme.colors.grayscale.black};
+  display: flex;
+
+  :last-of-type {
+    border: 0px;
+  }
+`;
+
+export const CoverInfoTitle = styled.div`
+  border-right: 1px solid ${({ theme }) => theme.colors.grayscale.black};
+  width: 20%;
+  padding: 22px 0px;
+  text-align: center;
+`;
+
+export const CoverInfoContent = styled.div`
+  padding: 22px 0px;
+  text-align: center;
+  flex: 1;
 `;
