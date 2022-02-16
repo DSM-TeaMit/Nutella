76f77b30ecf8cdf45b70a7b40716881509441da5
KJG04 <freedom7113@gmail.com>
2022-02-16 11:50:14 +0900

    🍫 (#62) - add file content

--- nutella/src/components/Result/SubmitResult/styles.ts
+++ nutella/src/components/Result/SubmitResult/styles.ts
@@ -15,3 +15,54 @@ export const ResultInner = styled.div`
 export const ResultTitle = styled.div`
   font: ${({ theme }) => theme.fonts.subtitle2};
 `;
+
+export const FileRow = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const FileTitle = styled.span`
+  font: ${({ theme }) => theme.fonts.body3};
+  color: ${({ theme }) => theme.colors.grayscale.black};
+  margin-right: 16px;
+`;
+
+export const FileSize = styled.span`
+  font: ${({ theme }) => theme.fonts.body3};
+  color: ${({ theme }) => theme.colors.grayscale.gray2};
+`;
+
+export const RemoveFile = styled.button`
+  border: 0;
+  background-color: transparent;
+  padding: 0px;
+  font: ${({ theme }) => theme.fonts.body3};
+  color: ${({ theme }) => theme.colors.red.default};
+  cursor: pointer;
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.red.hover};
+  }
+
+  &:active {
+    color: ${({ theme }) => theme.colors.red.click};
+  }
+`;
+
+export const AddFile = styled.button`
+  font: ${({ theme }) => theme.fonts.body3};
+  color: ${({ theme }) => theme.colors.grayscale.gray2};
+  border: 0;
+  background-color: transparent;
+  padding: 0px;
+  cursor: pointer;
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.grayscale.gray1};
+  }
+
+  &:active {
+    color: ${({ theme }) => theme.colors.grayscale.gray2};
+  }
+`;
