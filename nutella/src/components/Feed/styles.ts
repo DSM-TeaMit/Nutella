5da77583f787eb90e093768bb9b18593ddac9d01
kky04994 <kkyo8747@naver.com>
2022-02-19 23:55:13 +0900

    ➕(#27) add feed file

---
+++ nutella/src/components/Feed/styles.ts
@@ -0,0 +1,30 @@
+import styled from "@emotion/styled";
+import { colors } from "../../utils/theme/theme";
+
+export const Container = styled.div`
+  width: 100%;
+  min-height: 600px;
+  margin-top: 130px;
+  margin-bottom: 200px;
+`;
+
+export const FeedContent = styled.div`
+  margin: 0px 320px;
+`;
+
+export const TitleBox = styled.div`
+  display: flex;
+`;
+
+export const Title = styled.div<{ click: boolean }>`
+  font: ${({ theme }) => theme.fonts.h2};
+  color: ${(props) =>
+    props.click ? colors.grayscale.black : colors.grayscale.gray1};
+  margin-right: 24px;
+  cursor: pointer;
+  display: flex;
+`;
+
+export const ElementBox = styled.div`
+  margin: 48px 0px 96px 0px;
+`;
