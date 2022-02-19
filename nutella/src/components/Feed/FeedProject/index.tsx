65b4e45c2962e6579e874e3cce356720cdee7103
kky04994 <kkyo8747@naver.com>
2022-02-19 23:55:33 +0900

    ➕(#27) add feedproject file

---
+++ nutella/src/components/Feed/FeedProject/index.tsx
@@ -0,0 +1,19 @@
+import Project from "../../Project/Project";
+import * as S from "./styles";
+import { FC } from "react";
+
+interface PropsType {
+  total: number;
+}
+
+const FeedProject: FC<PropsType> = ({ total }) => {
+  return (
+    <S.ProjectBox>
+      {new Array(total).fill(0).map(() => (
+        <Project />
+      ))}
+    </S.ProjectBox>
+  );
+};
+
+export default FeedProject;
---
+++ nutella/src/components/Feed/FeedProject/styles.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const ProjectBox = styled.div`
+  margin-top: 36px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  column-gap: 20px;
+  row-gap: 50px;
+`;
