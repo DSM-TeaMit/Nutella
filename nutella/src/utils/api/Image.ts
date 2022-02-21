0d8b91727b0980f20a9c3e0172a1579f4cbf2ca5
KJG04 <freedom7113@gmail.com>
2022-02-22 01:21:03 +0900

    ➕️ (#54) - add image api file

---
+++ nutella/src/utils/api/Image.ts
@@ -0,0 +1,19 @@
+import Uri from "../../constant/Uri";
+import request from "../axios";
+
+export const postImage = async (file: File) => {
+  const formData = new FormData();
+  formData.append("image", file);
+
+  try {
+    const response = await request.post(
+      Uri.uploadImage.get({
+        projectUuid: "a08c9142-2e64-4702-891e-8561933fdc96",
+      }),
+      formData
+    );
+    console.log(response);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
