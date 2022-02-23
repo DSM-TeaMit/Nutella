9600004296d0ab84c0201b6e188033b6cd57f4bc
KJG04 <freedom7113@gmail.com>
2022-02-24 00:49:56 +0900

    🍫 (#54) - implement image component

--- nutella/src/components/MarkdownEditor/Image/index.tsx
+++ nutella/src/components/MarkdownEditor/Image/index.tsx
@@ -1,5 +1,13 @@
-import { FC } from "react";
-import { Row } from "../../../context/MarkdownCotext";
+import {
+  FC,
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { MarkdownContext, Row } from "../../../context/MarkdownCotext";
+import Img from "../../Img";
 import * as S from "./styles";
 
 interface PropsType {
@@ -7,9 +15,46 @@ interface PropsType {
 }
 
 const Image: FC<PropsType> = ({ item }) => {
+  const [isSelected, setIsSelected] = useState<boolean>(false);
+  const ref = useRef<HTMLDivElement>(null);
+  const { id } = item;
+  const { removeRowById } = useContext(MarkdownContext);
+
+  const onClick = (e: MouseEvent) => {
+    setIsSelected(
+      (ref.current && ref.current.contains(e.target as Node)) || false
+    );
+  };
+
+  const onKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      if (!isSelected) {
+        return;
+      }
+
+      if (["Backspace", "Delete"].includes(e.key)) {
+        removeRowById(id);
+      }
+    },
+    [removeRowById, isSelected, id]
+  );
+
+  useEffect(() => {
+    document.addEventListener("click", onClick);
+    document.addEventListener("keydown", onKeyDown);
+
+    return () => {
+      document.removeEventListener("click", onClick);
+      document.removeEventListener("keydown", onKeyDown);
+    };
+  }, [onKeyDown]);
+
   return (
     <S.Container>
-      <S.Image src={item.text} />
+      <S.ImgContainer ref={ref} onKeyDown={() => console.log("keydown")}>
+        <S.Image src={item.text} />
+        {isSelected && <S.Overlay />}
+      </S.ImgContainer>
     </S.Container>
   );
 };
--- nutella/src/components/MarkdownEditor/Image/styles.ts
+++ nutella/src/components/MarkdownEditor/Image/styles.ts
@@ -1,14 +1,32 @@
 import styled from "@emotion/styled";
 import Img from "../../Img";
 
-export const Image = styled(Img)`
-  border-radius: 10px;
-  width: 60%;
-`;
-
 export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 16px 0px;
+`;
+
+export const ImgContainer = styled.div`
+  border-radius: 10px;
+  width: 60%;
+  position: relative;
+  overflow: hidden;
+  cursor: pointer;
+`;
+
+export const Image = styled(Img)`
+  width: 100%;
+  display: block;
+`;
+
+export const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  mix-blend-mode: multiply;
+  background-color: ${({ theme }) => theme.colors.primary.default}50;
 `;
