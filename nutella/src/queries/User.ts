aeefece1801f68a7ceb8abfd8eb0b5ee9c0426f0
KJG04 <freedom7113@gmail.com>
2022-02-24 13:18:31 +0900

    🍫 (#79) - divide get profile

--- nutella/src/queries/User.ts
+++ nutella/src/queries/User.ts
@@ -1,8 +1,17 @@
 import { useQuery } from "react-query";
-import { getProfile, getUserProjects, getUserReports } from "../utils/api/User";
+import {
+  getProfile,
+  getUserProjects,
+  getUserReports,
+  MyProfileType,
+  UserProfileType,
+} from "../utils/api/User";
+
+export const useMyProfile = (userUuid: string) =>
+  useQuery(["profile", userUuid], () => getProfile<MyProfileType>(userUuid));
 
 export const useUserProfile = (userUuid: string) =>
-  useQuery(["profile", userUuid], () => getProfile(userUuid));
+  useQuery(["profile", userUuid], () => getProfile<UserProfileType>(userUuid));
 
 export const useUserProject = (userUuid: string) =>
   useQuery(["projects", userUuid], () => getUserProjects(userUuid));
