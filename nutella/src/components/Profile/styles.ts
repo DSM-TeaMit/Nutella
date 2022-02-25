59a552e96d55063d1cb57557ccbb2b306a74ee14
KJG04 <freedom7113@gmail.com>
2022-02-25 22:58:03 +0900

    💣 (#88) - remove useless style

--- nutella/src/components/Profile/styles.ts
+++ nutella/src/components/Profile/styles.ts
@@ -112,19 +112,6 @@ export const Github = styled.a`
   }
 `;
 
-export const Description = styled.div`
-  font: ${({ theme }) => theme.fonts.description};
-  color: ${({ theme }) => theme.colors.grayscale.gray2};
-  margin-bottom: 12px;
-`;
-
-export const ReadMe = styled.div`
-  padding: 24px;
-  border-radius: 10px;
-  background-color: ${({ theme }) => theme.colors.grayscale.white};
-  border: solid 1px ${({ theme }) => theme.colors.grayscale.lightGray1};
-`;
-
 export const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
