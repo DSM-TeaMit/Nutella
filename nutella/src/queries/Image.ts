564c705a291dfc0002a88263ad108b5b34799fcf
KJG04 <freedom7113@gmail.com>
2022-02-25 15:18:08 +0900

    🍫 (#84) - implement use image

--- nutella/src/queries/Image.ts
+++ nutella/src/queries/Image.ts
@@ -1,5 +1,8 @@
-import { useMutation } from "react-query";
-import { postImage } from "../utils/api/Image";
+import { useMutation, useQuery } from "react-query";
+import { getImage, postImage } from "../utils/api/Image";
 
 export const useImageMutation = (projectUuid: string) =>
   useMutation((file: File) => postImage(file, projectUuid));
+
+export const useImage = (src: string) =>
+  useQuery(["image", src], () => getImage(src));
