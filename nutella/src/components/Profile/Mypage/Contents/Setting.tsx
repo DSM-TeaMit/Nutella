c95f2bf8bdff70a3a2a9498869a259a5661329d7
KJG04 <freedom7113@gmail.com>
2022-02-26 20:54:36 +0900

    🍫 (#88) - add modal in setting

--- nutella/src/components/Profile/Mypage/Contents/Setting.tsx
+++ nutella/src/components/Profile/Mypage/Contents/Setting.tsx
@@ -2,10 +2,27 @@ import { ContentInner } from "../../styles";
 import * as S from "./styles";
 import { ArrowBlackIcons } from "../../../../assets/icons";
 import { useMyProfile } from "../../../../queries/User";
+import { Fragment, useCallback } from "react";
+import ModalPortal, { ModalPoralRef } from "../../../ModalPortal";
+import useModalRef from "../../../../hooks/useModalRef";
+import GithubIdModal from "../../../Modals/GithubIdModal";
 
 const Setting = () => {
   const { data, isError, isLoading } = useMyProfile(
     "e973c27b-3e0e-4863-86be-b2e0dfd24908"
+  );
+  const githubModalRef = useModalRef();
+  const deleteUserModalRef = useModalRef();
+
+  const openModal = useCallback(
+    (ref: React.RefObject<ModalPoralRef>) =>
+      (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.stopPropagation();
+        e.preventDefault();
+
+        ref.current?.show();
+      },
+    []
   );
 
   if (isError || isLoading) {
@@ -13,36 +30,44 @@ const Setting = () => {
   }
 
   const { githubId } = data!.data;
-
+  
   return (
-    <ContentInner>
-      <S.SettingContaienr>
-        <div>
-          <S.Title>계정 설정</S.Title>
-          <S.ContentContainer>
-            <S.SettingTitle>Github</S.SettingTitle>
-            <S.GrayButton>
-              <S.Button>
-                <span>{githubId || "입력하지 않음"}</span>
-                <img alt="github arrow" src={ArrowBlackIcons} />
-              </S.Button>
-            </S.GrayButton>
-          </S.ContentContainer>
-        </div>
-        <div>
-          <S.Subtitle>위험</S.Subtitle>
-          <S.ContentContainer>
-            <S.SettingTitleRed>계정 삭제</S.SettingTitleRed>
-            <S.RedButton>
-              <S.Button>
-                <span>삭제</span>
-                <img alt="github arrow" src={ArrowBlackIcons} />
-              </S.Button>
-            </S.RedButton>
-          </S.ContentContainer>
-        </div>
-      </S.SettingContaienr>
-    </ContentInner>
+    <Fragment>
+      <ContentInner>
+        <S.SettingContaienr>
+          <div>
+            <S.Title>계정 설정</S.Title>
+            <S.ContentContainer>
+              <S.SettingTitle>Github</S.SettingTitle>
+              <S.GrayButton onClick={openModal(githubModalRef)}>
+                <S.Button>
+                  <span>{githubId || "입력하지 않음"}</span>
+                  <img alt="github arrow" src={ArrowBlackIcons} />
+                </S.Button>
+              </S.GrayButton>
+            </S.ContentContainer>
+          </div>
+          <div>
+            <S.Subtitle>위험</S.Subtitle>
+            <S.ContentContainer>
+              <S.SettingTitleRed>계정 삭제</S.SettingTitleRed>
+              <S.RedButton onClick={openModal(deleteUserModalRef)}>
+                <S.Button>
+                  <span>삭제</span>
+                  <img alt="github arrow" src={ArrowBlackIcons} />
+                </S.Button>
+              </S.RedButton>
+            </S.ContentContainer>
+          </div>
+        </S.SettingContaienr>
+      </ContentInner>
+      <ModalPortal ref={githubModalRef}>
+        <GithubIdModal />
+      </ModalPortal>
+      <ModalPortal ref={deleteUserModalRef}>
+        <div>hello world!</div>
+      </ModalPortal>
+    </Fragment>
   );
 };
 
