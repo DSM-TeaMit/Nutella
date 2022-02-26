d2e7854bf5d1808b651c62f8de0102160691f8b4
KJG04 <freedom7113@gmail.com>
2022-02-26 10:13:40 +0900

    🍫 (#88) - use user report in report page

--- nutella/src/components/Profile/Mypage/Contents/Report.tsx
+++ nutella/src/components/Profile/Mypage/Contents/Report.tsx
@@ -1,13 +1,32 @@
 import ReportAccordion from "./ReportAccordion";
 import * as I from "../..//styles";
+import { useUserReports } from "../../../../queries/User";
 
 const Report = () => {
+  const { data, isError, isLoading } = useUserReports(
+    "e973c27b-3e0e-4863-86be-b2e0dfd24908"
+  );
+
+  if (isError && isLoading) {
+    return <></>;
+  }
+
+  const { accepted, pending, rejected } = data!.data;
+
   return (
     <I.ContentInner>
       <I.FlexContainer>
-        <ReportAccordion title="승인 대기중인" count={12} />
-        <ReportAccordion title="승인 거절된" count={12} />
-        <ReportAccordion title="승인 된" count={12} />
+        <ReportAccordion
+          title="승인 대기중인"
+          data={pending}
+          status="ACCEPTED"
+        />
+        <ReportAccordion
+          title="승인 거절된"
+          data={rejected}
+          status="DECLINED"
+        />
+        <ReportAccordion title="승인 된" data={accepted} status="PENDING" />
       </I.FlexContainer>
     </I.ContentInner>
   );
