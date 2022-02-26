39f88b401f8cf98db29a2195df0af316e896b701
KJG04 <freedom7113@gmail.com>
2022-02-26 16:18:34 +0900

    🍫 (#88) - render when length >= 0

--- nutella/src/components/Profile/Mypage/Contents/Profile.tsx
+++ nutella/src/components/Profile/Mypage/Contents/Profile.tsx
@@ -60,17 +60,19 @@ const Profile: FC<PropsType> = ({ data: queryData }) => {
           <I.Line />
         </I.ProfileContainerOuter>
         <GithubReadme githubId={githubId} />
-        <div>
-          <I.ContentTitle>
-            <I.H3>승인 대기중인 보고서&nbsp;</I.H3>
-            <I.BlueH3>{pendingCount}</I.BlueH3>
-          </I.ContentTitle>
-          <I.Grid>
-            {pendingReports.map((value) => (
-              <ReportCard key={value.uuid} data={value} />
-            ))}
-          </I.Grid>
-        </div>
+        {pendingCount !== 0 && (
+          <div>
+            <I.ContentTitle>
+              <I.H3>승인 대기중인 보고서&nbsp;</I.H3>
+              <I.BlueH3>{pendingCount}</I.BlueH3>
+            </I.ContentTitle>
+            <I.Grid>
+              {pendingReports.map((value) => (
+                <ReportCard key={value.uuid} data={value} />
+              ))}
+            </I.Grid>
+          </div>
+        )}
         <div>
           <I.ContentTitle>
             <I.H3>프로젝트&nbsp;</I.H3>
