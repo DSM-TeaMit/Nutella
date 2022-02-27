7f9ef841cd3325daa717584cd1b5983567adabad
KJG04 <freedom7113@gmail.com>
2022-02-27 15:30:35 +0900

    🍫 (#92) - add delete comment query

--- nutella/src/queries/Comment.ts
+++ nutella/src/queries/Comment.ts
@@ -1,6 +1,11 @@
 import { useMutation, useQuery, useQueryClient } from "react-query";
 import CommentSource from "../interface/CommentSource";
-import { getComment, postComment, PostComment } from "../utils/api/Comment";
+import {
+  deleteComment,
+  getComment,
+  postComment,
+  PostComment,
+} from "../utils/api/Comment";
 
 export const useComment = (projectUuid: string, type: CommentSource) =>
   useQuery(["comment", projectUuid, type], () => getComment(projectUuid, type));
@@ -11,6 +16,16 @@ export const useCommentMutation = (projectUuid: string) => {
   return useMutation((data: PostComment) => postComment(projectUuid, data), {
     onSuccess: (_, variables) => {
       queryClient.invalidateQueries(["comment", projectUuid, variables.type]);
+    },
+  });
+};
+
+export const useDeleteComment = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation((commentUuid: string) => deleteComment(commentUuid), {
+    onSuccess: () => {
+      queryClient.invalidateQueries("comment");
     },
   });
 };
