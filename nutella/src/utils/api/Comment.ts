1b0496657ee2c35e72e56bcdcb267672c65817e2
KJG04 <freedom7113@gmail.com>
2022-02-27 15:15:16 +0900

    🍫 (#92) - implement get comment api

--- nutella/src/utils/api/Comment.ts
+++ nutella/src/utils/api/Comment.ts
@@ -1,3 +1,23 @@
+import Uri from "../../constant/Uri";
 import CommentSource from "../../interface/CommentSource";
+import request from "../axios";
 
-export const getComment = async (uuid: string, type: CommentSource) => {};
+interface CommentType {
+  uuid: string;
+  writerId: string;
+  writerType: string;
+  writerName: string;
+  writerSno: number;
+  content: string;
+}
+
+interface CommentList {
+  count: number;
+  comments: CommentType[];
+}
+
+export const getComment = async (projectUuid: string, type: CommentSource) => {
+  const uri = Uri.projectComment.get({ projectUuid });
+
+  return await request.get<CommentList>(uri, { params: { type } });
+};
