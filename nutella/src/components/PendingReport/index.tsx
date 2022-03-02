180ad0922e834b6538478edc6150d746fa1e7111
KJG04 <freedom7113@gmail.com>
2022-03-02 13:30:13 +0900

    🍫 (#98) - use pending report card

--- nutella/src/components/PendingReport/index.tsx
+++ nutella/src/components/PendingReport/index.tsx
@@ -1,3 +1,4 @@
+import PendingReportCard from "../PendingReportCard";
 import * as S from "./styles";
 
 const PendingReport = () => {
@@ -7,7 +8,13 @@ const PendingReport = () => {
         <S.Title>승인 요청 보고서&nbsp;</S.Title>
         <S.Count>12</S.Count>
       </div>
-      <S.List></S.List>
+      <S.List>
+        <PendingReportCard />
+        <PendingReportCard />
+        <PendingReportCard />
+        <PendingReportCard />
+        <PendingReportCard />
+      </S.List>
     </S.Container>
   );
 };
