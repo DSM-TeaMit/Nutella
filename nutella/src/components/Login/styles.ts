7f8eedbfb63f20a57db7a130f5c05328339ec157
kky04994 <kkyo8747@naver.com>
2022-03-04 15:35:27 +0900

    🍫 (#106) - delete unnecessary code

--- nutella/src/components/Login/styles.ts
+++ nutella/src/components/Login/styles.ts
@@ -13,12 +13,11 @@ export const LoginContent = styled.div`
 
 export const Logo = styled.img`
   height: 65px;
-  margin-bottom: 45px;
+  margin-bottom: 48px;
 `;
 
 export const OauthBox = styled.div`
   width: 334px;
-  height: 303px;
   padding: 0;
 `;
 
@@ -38,7 +37,8 @@ export const Title = styled.div`
 export const OauthBtn = styled.div`
   width: 330px;
   height: 40px;
-  margin-top: 23px;
+  margin-top: 24px;
+  margin-bottom: 28px;
   padding: 5px 16px;
   font: ${({ theme }) => theme.fonts.body2};
   color: ${({ theme }) => theme.colors.grayscale.black};
@@ -49,20 +49,6 @@ export const OauthBtn = styled.div`
   img {
     vertical-align: middle;
     margin-right: 43px;
-  }
-`;
-
-export const Divider = styled.div`
-  width: 334px;
-  height: 23px;
-  margin: 34px 0;
-  span {
-    font: ${({ theme }) => theme.fonts.body3};
-    color: ${({ theme }) => theme.colors.grayscale.gray1};
-    margin: 0 7px;
-  }
-  img {
-    vertical-align: middle;
   }
 `;
 
