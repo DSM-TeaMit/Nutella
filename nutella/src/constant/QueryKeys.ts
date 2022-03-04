2c2ae4b15a8d14200fdfa94d7c1d9736be01053c
KJG04 <freedom7113@gmail.com>
2022-03-04 22:03:05 +0900

    🍫 (#113) - add query key constant & use query key

---
+++ nutella/src/constant/QueryKeys.ts
@@ -0,0 +1,15 @@
+const queryKeys = {
+  comment: "comment",
+  image: "image",
+  planDetail: "plan_detail",
+  result: "result",
+  googleOauth: "sign",
+  githubOauth: "github_oauth",
+  profile: "profile",
+  my: "my",
+  projects: "projects",
+  reports: "reports",
+  readme: "readme",
+} as const;
+
+export default queryKeys;
--- nutella/src/queries/Signup.ts
+++ nutella/src/queries/Signup.ts
@@ -2,6 +2,7 @@ import { AxiosResponse } from "axios";
 import { useCallback } from "react";
 import { useMutation, useQueryClient, useQuery } from "react-query";
 import { useNavigate } from "react-router-dom";
+import queryKeys from "../constant/QueryKeys";
 import storageKeys from "../constant/StorageKeys";
 import useMessageContext from "../hooks/useMessageContext";
 import {
@@ -24,8 +25,8 @@ export const useOauthGoogle = (code: string | null) => {
 
   const onSuccess = useCallback((data: AxiosResponse<TokenType, any>) => {
     const { accessToken, refreshToken } = data.data;
-    localStorage.setItem("access_token", accessToken);
-    localStorage.setItem("refresh_token", refreshToken);
+    localStorage.setItem(storageKeys.accessToken, accessToken);
+    localStorage.setItem(storageKeys.accessToken, refreshToken);
     localStorage.setItem(
       storageKeys.expireAt,
       getDateWithAddHour(24).toString()
@@ -42,22 +43,15 @@ export const useOauthGoogle = (code: string | null) => {
     navigate("/");
   }, [showMessage, navigate]);
 
-  return useQuery(["sign_up", code], () => getOauthGoogle(code), {
+  return useQuery([queryKeys.googleOauth, code], () => getOauthGoogle(code), {
     onSuccess,
     onError,
     retry: false,
   });
 };
 
-export const useUserInfo = () => {
-  const queryClient = useQueryClient();
-
-  return useMutation((data: InfoType) => postUserInfo(data), {
-    onSuccess: () => {
-      queryClient.invalidateQueries(["info"]);
-    },
-  });
-};
+export const useUserInfo = () =>
+  useMutation((data: InfoType) => postUserInfo(data));
 
 export const useOauthGithubSignup = (code: string | null) =>
-  useQuery(["GithubSignup", code], () => getOauthGithub(code));
+  useQuery([queryKeys.githubOauth, code], () => getOauthGithub(code));
