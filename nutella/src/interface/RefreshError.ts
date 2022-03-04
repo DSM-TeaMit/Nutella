8692ae6aca945474f3d8b60bb4a4d200b1edaf35
KJG04 <freedom7113@gmail.com>
2022-03-04 11:20:47 +0900

    🍫 (#112) - implement refresh token

---
+++ nutella/src/interface/RefreshError.ts
@@ -0,0 +1,15 @@
+export type ErrorType = "NO_TOKEN" | "EXPIRED_TOKEN" | "NETWORK_ERROR";
+
+const messageMap = new Map<ErrorType, string>()
+  .set("NO_TOKEN", "토큰이 존재하지 않습니다. 다시 로그인 해주세요.")
+  .set("EXPIRED_TOKEN", "로그인이 만료되었습니다. 다시 로그인 해주세요.")
+  .set("NETWORK_ERROR", "연결 실패했습니다. 다시 로그인 해주세요.");
+
+class RefreshError extends Error {
+  constructor(type: ErrorType) {
+    super(messageMap.get(type)!);
+    this.name = "RefreshError";
+  }
+}
+
+export default RefreshError;
--- nutella/src/utils/axios/refresh.ts
+++ nutella/src/utils/axios/refresh.ts
@@ -3,6 +3,7 @@ import { instance } from ".";
 import baseURL from "../../constant/BaseUrl";
 import storageKeys from "../../constant/StorageKeys";
 import Uri from "../../constant/Uri";
+import RefreshError from "../../interface/RefreshError";
 
 export const request = axios.create({
   baseURL,
@@ -24,40 +25,46 @@ export const refresh = async (
   config: AxiosRequestConfig
 ): Promise<AxiosRequestConfig> => {
   const expireAt = localStorage.getItem(storageKeys.expireAt);
-  const accessToken = localStorage.getItem(storageKeys.accessToken);
+  let accessToken = localStorage.getItem(storageKeys.accessToken);
   const refreshToken = localStorage.getItem(storageKeys.refreshToken);
 
   if (!refreshToken || !expireAt) {
     //리프레시 토큰이 없거나 만료 기간이 로컬 스토리지에 없을때
-    window.location.href = "/"; //로그인으로 보내고 스토리지를 비운다
-    localStorage.removeItem(storageKeys.accessToken);
-    localStorage.removeItem(storageKeys.refreshToken);
-    localStorage.removeItem(storageKeys.expireAt);
 
-    return config;
+    throw new RefreshError("NO_TOKEN");
   }
 
   const uri = Uri.refresh.get();
 
-  try {
-    const { accessToken: newAccessToken, refreshToken: newRefreshToken } = (
-      await instance.put<Token>(uri, { refreshToken })
-    ).data;
+  if (new Date() >= new Date(expireAt)) {
+    //현재 시간이 엑세스 토큰 만료 시간을 지났을 때
 
-    localStorage.setItem(storageKeys.accessToken, newAccessToken);
-    localStorage.setItem(storageKeys.refreshToken, newRefreshToken);
-    localStorage.setItem(
-      storageKeys.expireAt,
-      getDateWithAddHour(2).toString()
-    );
-  } catch {
-    window.location.href = "/";
-    localStorage.removeItem(storageKeys.accessToken);
-    localStorage.removeItem(storageKeys.refreshToken);
-    localStorage.removeItem(storageKeys.expireAt);
+    try {
+      const { accessToken: newAccessToken, refreshToken: newRefreshToken } = (
+        await instance.put<Token>(uri, { refreshToken })
+      ).data;
 
-    return config;
+      localStorage.setItem(storageKeys.accessToken, newAccessToken);
+      localStorage.setItem(storageKeys.refreshToken, newRefreshToken);
+      localStorage.setItem(
+        storageKeys.expireAt,
+        getDateWithAddHour(24).toString()
+      );
+
+      accessToken = newAccessToken;
+    } catch (error) {
+      //토큰 리프레시시 오류 발생 (토큰 만료 등)
+      if (axios.isAxiosError(error)) {
+        if (error.response && error.response?.status === 401) {
+          //토큰 만료
+          throw new RefreshError("EXPIRED_TOKEN");
+        }
+      }
+      //다른 오류
+      throw new RefreshError("NETWORK_ERROR");
+    }
   }
+
   config.headers!["Authorization"] = `Bearer ${accessToken}`;
 
   return config;
