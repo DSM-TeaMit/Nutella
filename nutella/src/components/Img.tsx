fcacdddbb1c3ff810179be82ffd448d88c252395
KJG04 <freedom7113@gmail.com>
2022-03-05 16:16:36 +0900

    🍫 (#119) - implement is loading & error form

--- nutella/src/components/Img.tsx
+++ nutella/src/components/Img.tsx
@@ -1,22 +1,47 @@
 import { FC, useMemo } from "react";
 import { useImage } from "../queries/Image";
+import Twemoji from "react-twemoji";
+
+type PropsType = ImageProps & { emoji?: string };
 
 type ImageProps = React.DetailedHTMLProps<
   React.ImgHTMLAttributes<HTMLImageElement>,
   HTMLImageElement
 >;
 
-const Img: FC<ImageProps> = (props) => {
-  const propsWithoutSomething: Omit<ImageProps, "src"> = useMemo(
+const Img: FC<PropsType> = (props) => {
+  const propsWithoutSomething: Omit<PropsType, "src" | "emoji"> = useMemo(
     () => ({
       ...props,
     }),
     [props]
   );
 
-  const { src } = props;
+  const { src, emoji } = props;
 
-  const { data } = useImage(src || "");
+  const { data, isError, isLoading } = useImage(src || "");
+
+  if (isLoading) {
+    return <img {...propsWithoutSomething} />;
+  }
+
+  if (emoji) {
+    return (
+      <Twemoji
+        options={{
+          folder: "svg",
+          ext: ".svg",
+          className: `${props.className} default-emoji`,
+        }}
+      >
+        {emoji}
+      </Twemoji>
+    );
+  }
+
+  if (isError) {
+    return <img {...propsWithoutSomething} />;
+  }
 
   return <img {...propsWithoutSomething} src={data} />;
 };
--- nutella/src/queries/Image.ts
+++ nutella/src/queries/Image.ts
@@ -2,4 +2,11 @@ import { useQuery } from "react-query";
 import { getImage } from "../utils/api/Image";
 
 export const useImage = (src: string) =>
-  useQuery(["image", src], () => getImage(src));
+  useQuery(["image", src], () => getImage(src), {
+    retry: false,
+    refetchInterval: false,
+    refetchOnReconnect: false,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchIntervalInBackground: false,
+  });
