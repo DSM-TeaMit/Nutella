0e8e08fed6825967f62d3e1623f7f77ebaeb64a2
KJG04 <freedom7113@gmail.com>
2022-03-05 20:27:13 +0900

    🍫 (#125) - add management account container

---
+++ nutella/src/container/ManagementAccount.tsx
@@ -0,0 +1,7 @@
+import ManagementAccount from "../components/Profile/ManagementAccount";
+
+const ManagementAccountContainer = () => {
+  return <ManagementAccount />;
+};
+
+export default ManagementAccountContainer;
