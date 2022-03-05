ebd208be6d894f6c5ebbdfac9d794d457ff92df9
KJG04 <freedom7113@gmail.com>
2022-03-05 20:04:03 +0900

    ➕️ (#125) - add admin side bar component

---
+++ nutella/src/components/SideBar/Admin.tsx
@@ -0,0 +1,32 @@
+import { FC } from "react";
+import NavigationType from "../../interface/Navigation";
+import Navigation from "./Navigation";
+import * as S from "./styles";
+
+interface PropsType {
+  navs: NavigationType[];
+}
+
+const AdminSideBar: FC<PropsType> = ({ navs }) => {
+  return (
+    <S.Container>
+      <S.InfoContainer>
+        <S.ProfileImage />
+        <S.TextContainer>
+          <S.Name>김진근 선생님</S.Name>
+        </S.TextContainer>
+      </S.InfoContainer>
+      <S.Line />
+      <S.NavContainer>
+        {navs.map(({ to, text, icon }, index) => (
+          <Navigation end to={to} key={index}>
+            <img alt="profile" src={icon} />
+            <div>{text}</div>
+          </Navigation>
+        ))}
+      </S.NavContainer>
+    </S.Container>
+  );
+};
+
+export default AdminSideBar;
