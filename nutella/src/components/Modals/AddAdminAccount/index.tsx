ddbcbe6dafc3b60e99c18bdecbd560ddbd9d42a2
KJG04 <freedom7113@gmail.com>
2022-03-05 20:59:13 +0900

    🍫 (#125) - add admin account ui

--- nutella/src/components/Modals/AddAdminAccount/index.tsx
+++ nutella/src/components/Modals/AddAdminAccount/index.tsx
@@ -1,6 +1,31 @@
+import BlueButton from "../../Buttons/BlueButton";
+import BorderButton from "../../Buttons/BorderButton";
+import Input from "../../Input";
+import * as S from "./styles";
 
 const AddAdminAccountModal = () => {
-
-}
+  return (
+    <S.Container>
+      <S.Inner>
+        <S.ContentContainer>
+          <S.Subtitle>이름</S.Subtitle>
+          <Input placeholder="이름을 입력해주세요..." />
+        </S.ContentContainer>
+        <S.ContentContainer>
+          <S.Subtitle>비밀번호</S.Subtitle>
+          <Input placeholder="비밀번호를 입력해주세요..." />
+        </S.ContentContainer>
+        <S.ContentContainer>
+          <S.Subtitle>비밀번호 확인</S.Subtitle>
+          <Input placeholder="비밀번호를 입력해주세요..." />
+        </S.ContentContainer>
+      </S.Inner>
+      <S.ButtonContainer>
+        <BlueButton>추가</BlueButton>
+        <BorderButton>취소</BorderButton>
+      </S.ButtonContainer>
+    </S.Container>
+  );
+};
 
 export default AddAdminAccountModal;
--- nutella/src/components/Modals/AddAdminAccount/styles.ts
+++ nutella/src/components/Modals/AddAdminAccount/styles.ts
@@ -1,1 +1,42 @@
-export {};
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  padding: 24px 32px;
+  background-color: ${({ theme }) => theme.colors.grayscale.white};
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  row-gap: 36px;
+  min-width: 550px;
+  width: 550px;
+  max-width: 550px;
+`;
+
+export const Inner = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 40px;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: right;
+  column-gap: 16px;
+`;
+
+export const Title = styled.div`
+  font: ${({ theme }) => theme.fonts.h2};
+  color: ${({ theme }) => theme.colors.grayscale.black};
+`;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 12px;
+  width: 100%;
+`;
+
+export const Subtitle = styled.div`
+  font: ${({ theme }) => theme.fonts.body3};
+  color: ${({ theme }) => theme.colors.grayscale.gray2};
+`;
