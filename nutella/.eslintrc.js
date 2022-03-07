f77ab1271966b89583d3496f1d7409d4ab640430
KJG04 <freedom7113@gmail.com>
2022-03-07 10:29:23 +0900

    🍫 (#134) - add eslint

---
+++ nutella/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "react-hooks"],
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-key": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "prefer-const": "off",
+    "no-constant-condition": "off",
+    "react/display-name": "off",
+    "@typescript-eslint/ban-types": "off",
+  },
+};
