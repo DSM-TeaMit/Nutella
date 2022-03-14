9dffaa658638d6441477b86bb695282d24c589f3
KJG04 <freedom7113@gmail.com>
2022-03-14 20:07:13 +0900

    🍫 (#131) - change plan type

--- nutella/src/utils/api/Plan.ts
+++ nutella/src/utils/api/Plan.ts
@@ -2,19 +2,34 @@ import { AxiosResponse } from "axios";
 import { getInitRows } from "../../components/MarkdownEditor";
 import Uri from "../../constant/Uri";
 import { Row } from "../../context/MarkdownCotext";
+import ProjectTypes from "../../interface/ProjectTypes";
 import request from "../axios";
 
+export interface Includes {
+  report: boolean;
+  code: boolean;
+  outcome: boolean;
+  others: string | undefined;
+}
+
+export interface Member {
+  studentNo: number;
+  name: string;
+}
+
+export type Requestor = "USER_EDITABLE" | "USER_NON_EDITABLE" | "ADMIN";
+
 interface PlanType {
+  projectName: string;
+  projectType: ProjectTypes;
   startDate: string;
   endDate: string;
+  requestorType: Requestor;
+  writer: Member;
+  members: Member[];
   goal: string;
   content: string;
-  includes: {
-    report: boolean;
-    code: boolean;
-    outcome: boolean;
-    others: string | undefined;
-  };
+  includes: Includes;
 }
 
 interface Uuid {
@@ -22,16 +37,32 @@ interface Uuid {
 }
 
 export interface ParsedPlanType {
+  projectName: string;
+  projectType: ProjectTypes;
+  startDate: Date;
+  endDate: Date;
+  requestorType: Requestor;
+  writer: Member;
+  members: Member[];
+  goal: Row[];
+  content: Row[];
+  includes: Includes;
+}
+
+export interface ModifyPlan {
+  startDate: string;
+  endDate: string;
+  goal: string;
+  content: string;
+  includes: Includes;
+}
+
+export interface ParsedModifyPlan {
   startDate: Date;
   endDate: Date;
   goal: Row[];
   content: Row[];
-  includes: {
-    report: boolean;
-    code: boolean;
-    outcome: boolean;
-    others: string | undefined;
-  };
+  includes: Includes;
 }
 
 const dateToString = (date: Date) =>
@@ -45,12 +76,12 @@ export const createPlanReport = async (projectUuid: string) => {
 
   const dateString = dateToString(date);
 
-  return await request.post<Uuid, AxiosResponse<Uuid>, PlanType>(uri, {
+  return await request.post<Uuid, AxiosResponse<Uuid>, ModifyPlan>(uri, {
     startDate: dateString,
     endDate: dateString,
-    includes: { report: true, code: true, outcome: true, others: undefined },
     goal: JSON.stringify(getInitRows()),
     content: JSON.stringify(getInitRows()),
+    includes: { report: true, code: true, outcome: true, others: undefined },
   });
 };
 
@@ -61,6 +92,11 @@ export const getPlanReport = async (projectUuid: string) => {
   const { data } = response;
 
   const result: ParsedPlanType = {
+    projectName: data.projectName,
+    projectType: data.projectType,
+    members: data.members,
+    writer: data.writer,
+    requestorType: data.requestorType,
     startDate: new Date(data.startDate),
     endDate: new Date(data.endDate),
     goal: JSON.parse(data.goal) as Row[],
@@ -82,11 +118,11 @@ export const getPlanReport = async (projectUuid: string) => {
 
 export const modifyPlanReport = async (
   projectUuid: string,
-  data: ParsedPlanType
+  data: ParsedModifyPlan
 ) => {
   const uri = Uri.plan.get({ projectUuid });
 
-  const requestData: PlanType = {
+  const requestData: ModifyPlan = {
     startDate: dateToString(data.startDate),
     endDate: dateToString(data.endDate),
     goal: JSON.stringify(data.goal),
@@ -94,10 +130,11 @@ export const modifyPlanReport = async (
     includes: data.includes,
   };
 
-  return await request.patch<unknown, AxiosResponse<unknown, unknown>, PlanType>(
-    uri,
-    requestData
-  );
+  return await request.patch<
+    unknown,
+    AxiosResponse<unknown, unknown>,
+    ModifyPlan
+  >(uri, requestData);
 };
 
 export const submitPlanReport = async (projectUuid: string) => {
