92c2d3a92842a2d117971ac6fda81161f9fb3bb5
KJG04 <freedom7113@gmail.com>
2022-03-15 17:26:11 +0900

    😎 (#131) - fix type error

--- nutella/src/components/Modals/DatePicker/index.tsx
+++ nutella/src/components/Modals/DatePicker/index.tsx
@@ -6,8 +6,8 @@ import * as S from "./styles";
 import { ArrowIcons } from "../../../assets/icons";
 
 export interface DateState {
-  start: Date;
-  end: Date;
+  start: Date | undefined;
+  end: Date | undefined;
 }
 
 type DateName = keyof DateState;
@@ -45,7 +45,14 @@ const getCellType = (
 
   const { start: startDate, end: endDate } = dates;
 
-  if (compareDate(startDate, endDate) === 0 && compareDate(currentDate, startDate) === 0) {
+  if (!startDate || !endDate) {
+    return "default";
+  }
+
+  if (
+    compareDate(startDate, endDate) === 0 &&
+    compareDate(currentDate, startDate) === 0
+  ) {
     return "selected";
   }
 
@@ -59,7 +66,10 @@ const getCellType = (
     return "end";
   }
 
-  if (compareDate(currentDate, startDate) === 1 && compareDate(currentDate, endDate) === -1) {
+  if (
+    compareDate(currentDate, startDate) === 1 &&
+    compareDate(currentDate, endDate) === -1
+  ) {
     return "middle";
   }
 
@@ -73,13 +83,14 @@ const getCellType = (
   return "default";
 };
 
-const dateToYearMonth = (date: Date) => `${date.getFullYear()}년 ${date.getMonth() + 1}월`;
+const dateToYearMonth = (date: Date) =>
+  `${date.getFullYear()}년 ${date.getMonth() + 1}월`;
 
 export const DatePicker: FC<PropsType> = ({ datesState }) => {
   const initCalendarDate = () => {
     const [dates] = datesState;
     let d = new Date();
-    if (dates) {
+    if (dates && dates.start) {
       d = new Date(dates.start);
     }
     d.setDate(1);
@@ -103,7 +114,9 @@ export const DatePicker: FC<PropsType> = ({ datesState }) => {
   );
 
   const dateToString = (placeholder: string, date?: Date): string =>
-    date ? `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일` : placeholder;
+    date
+      ? `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`
+      : placeholder;
 
   const onTypeClick = (type: DateName) => () => setSelectedType(type);
 
@@ -128,13 +141,23 @@ export const DatePicker: FC<PropsType> = ({ datesState }) => {
       const selectedDate = displayDates[selectedType];
       let type = selectedType;
 
+      if (!selectedDate || !displayDates.start || !displayDates.end) {
+        return;
+      }
+
       if (compareDate(date, selectedDate) === 0) {
         return;
       }
 
-      if (selectedType === "start" && compareDate(date, displayDates.end) === 1) {
+      if (
+        selectedType === "start" &&
+        compareDate(date, displayDates.end) === 1
+      ) {
         type = "end";
-      } else if (selectedType === "end" && compareDate(date, displayDates.start) === -1) {
+      } else if (
+        selectedType === "end" &&
+        compareDate(date, displayDates.start) === -1
+      ) {
         type = "start";
       }
 
@@ -168,11 +191,17 @@ export const DatePicker: FC<PropsType> = ({ datesState }) => {
     <S.Container>
       <S.Title>날짜를 선택해주세요</S.Title>
       <S.Date>
-        <S.DateSpan isActive={selectedType === "start"} onClick={onTypeClick("start")}>
+        <S.DateSpan
+          isActive={selectedType === "start"}
+          onClick={onTypeClick("start")}
+        >
           {dateToString("시작", displayDates?.start)}
         </S.DateSpan>
         <span>&nbsp;~&nbsp;</span>
-        <S.DateSpan isActive={selectedType === "end"} onClick={onTypeClick("end")}>
+        <S.DateSpan
+          isActive={selectedType === "end"}
+          onClick={onTypeClick("end")}
+        >
           {dateToString("종료", displayDates?.end)}
         </S.DateSpan>
       </S.Date>
