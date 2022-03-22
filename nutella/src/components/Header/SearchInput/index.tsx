0c347ec4641f4dce56264bc9227cbe1a49bf20b3
김진근 <freedom7113@gmail.com>
2022-03-22 18:12:15 +0900

    🍫 (#97) - add search function

--- nutella/src/components/Header/SearchInput/index.tsx
+++ nutella/src/components/Header/SearchInput/index.tsx
@@ -1,11 +1,30 @@
+import { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 import useInput from "../../../hooks/useInput";
 import * as S from "./styles";
 
 const SearchInput = () => {
-  const [inputProps] = useInput("", true);
+  const [inputProps, [value, setValue]] = useInput("");
+  const navigate = useNavigate();
+
+  const onKeydown = useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>) => {
+      const v = value.toString().trim();
+
+      if (e.key === "Enter" && v.toString().length > 0) {
+        navigate(`/search?q=${v}`);
+        setValue("");
+      }
+    },
+    [navigate, setValue, value]
+  );
 
   return (
-    <S.Input {...inputProps} placeholder="검색어를 입력해주세요... (ex 유저 이름, 프로젝트 이름" />
+    <S.Input
+      onKeyDown={onKeydown}
+      {...inputProps}
+      placeholder="검색어를 입력해주세요... (ex 유저 이름, 프로젝트 이름"
+    />
   );
 };
 
