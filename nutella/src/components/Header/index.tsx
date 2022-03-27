953161d47ff502aa1ef0f30dae832266e14e5362
김진근 <freedom7113@gmail.com>
2022-03-27 21:48:19 +0900

    🍫 (#172) - add pending report link when admin

--- nutella/src/components/Header/index.tsx
+++ nutella/src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import * as S from "./styles";
 import { Logo } from "../../assets/logo";
-import { ArrowIcons } from "../../assets/icons";
+import { ArrowIcons, DocumentEditIcons } from "../../assets/icons";
 import { Link, useNavigate } from "react-router-dom";
 import SearchInput from "./SearchInput";
 import { useHeader } from "../../queries/User";
@@ -43,40 +43,48 @@ const Header = () => {
             <SearchInput />
           </S.SearchInputContainer>
         </S.FlexContainer>
-        <S.ProfileContainer>
-          {isLoading && <S.UserName>로딩중...</S.UserName>}
-          {isSuccess && (
-            <S.SLink
-              to={data?.data.type === "admin" ? "/admin-mypage" : "/mypage"}
-            >
-              <S.UserName>
-                {data?.data.studentNo} {data?.data.name}{" "}
-                {data?.data.type === "admin" && "선생님"}
-              </S.UserName>
-            </S.SLink>
+        <S.RightContainer>
+          {data?.data.type === "admin" && (
+            <S.Nav to="/pending-report">
+              <img src={DocumentEditIcons} alt="pending reports" />
+              <div>승인 요청 보고서</div>
+            </S.Nav>
           )}
-          {isError && <S.UserName>오류 발생</S.UserName>}
-          <S.UserImageContainer>
-            <S.SLink
-              to={data?.data.type === "admin" ? "/admin-mypage" : "/mypage"}
-            >
-              <S.UserImage
-                src={data?.data.thumbnailUrl}
-                emoji={data?.data.emoji}
-              />
-            </S.SLink>
-            <S.ArrowContainer>
-              <S.Arrow ref={ref} onClick={onArrowClick}>
-                <img
-                  src={ArrowIcons}
-                  alt="profile arrow"
-                  style={{ transform: `rotate(${isActive ? 180 : 0}deg)` }}
+          <S.ProfileContainer>
+            {isLoading && <S.UserName>로딩중...</S.UserName>}
+            {isSuccess && (
+              <S.SLink
+                to={data?.data.type === "admin" ? "/admin-mypage" : "/mypage"}
+              >
+                <S.UserName>
+                  {data?.data.studentNo} {data?.data.name}{" "}
+                  {data?.data.type === "admin" && "선생님"}
+                </S.UserName>
+              </S.SLink>
+            )}
+            {isError && <S.UserName>오류 발생</S.UserName>}
+            <S.UserImageContainer>
+              <S.SLink
+                to={data?.data.type === "admin" ? "/admin-mypage" : "/mypage"}
+              >
+                <S.UserImage
+                  src={data?.data.thumbnailUrl}
+                  emoji={data?.data.emoji}
                 />
-              </S.Arrow>
-              {isActive && <S.Logout onClick={onLogout}>로그아웃</S.Logout>}
-            </S.ArrowContainer>
-          </S.UserImageContainer>
-        </S.ProfileContainer>
+              </S.SLink>
+              <S.ArrowContainer>
+                <S.Arrow ref={ref} onClick={onArrowClick}>
+                  <img
+                    src={ArrowIcons}
+                    alt="profile arrow"
+                    style={{ transform: `rotate(${isActive ? 180 : 0}deg)` }}
+                  />
+                </S.Arrow>
+                {isActive && <S.Logout onClick={onLogout}>로그아웃</S.Logout>}
+              </S.ArrowContainer>
+            </S.UserImageContainer>
+          </S.ProfileContainer>
+        </S.RightContainer>
       </S.ContentContainer>
       <S.Line />
     </S.Container>
