6149d773b5d8f37058f028f0bb53c141f0a7a8e3
kky04994 <kkyo8747@naver.com>
2022-03-28 10:19:01 +0900

    🍫 (#178) - adminlogin code in admin file

--- nutella/src/utils/api/Admin.ts
+++ nutella/src/utils/api/Admin.ts
@@ -1,6 +1,15 @@
 import Uri from "../../constant/Uri";
 import request from "../axios";
 
+export interface LoginType {
+  id: string;
+  password: string;
+}
+
+export const postAdminLogin = async (data: LoginType) => {
+  return await request.post(Uri.login.get(), data);
+};
+
 export interface Accounts {
   count: number;
   accounts: AccountType[];
