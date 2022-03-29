b86a15f39e3106bb5e66a1506d3feb90046b5fce
kky04994 <kkyo8747@naver.com>
2022-03-29 10:19:15 +0900

    🍫 (#108) - menu code modify

--- nutella/src/components/Feed/index.tsx
+++ nutella/src/components/Feed/index.tsx
@@ -1,65 +1,41 @@
 import * as S from "./styles";
-import FeedTitleType from "../../interface/FeedTitle";
-import FeedProject from "./FeedProject";
 import { useState } from "react";
 import React from "react";
-
-interface PropsType {
-  total: number;
-}
+import { useFeed } from "../../queries/Feed";
+import Project from "../Cards/MainProjectCard";
 
 const Feed = () => {
-  const PopularFeed = 1;
-  const NewFeed = 2;
-  const [contentState, setContentState] = useState(PopularFeed);
-
-  const onPopularFeedClick = () => {
-    setContentState(PopularFeed);
-  };
-
-  const onNewFeedClick = () => {
-    setContentState(NewFeed);
-  };
-
-  const title: FeedTitleType[] = [
-    {
-      title: "인기있는 프로젝트",
-      onClick: onPopularFeedClick,
-    },
-    {
-      title: "최근 프로젝트",
-      onClick: onNewFeedClick,
-    },
+  const [currentTab, setCurrentTab] = useState(0);
+  const menuArr = [
+    { name: "인기있는 프로젝트", key: "popularity", content: <Project /> },
+    { name: "최신 프로젝트", key: "recently", content: <Project /> },
   ];
+  const { data, isError, isSuccess, isLoading } = useFeed();
 
-  const renderContent = (): JSX.Element => {
-    const contentMap = new Map<number, React.FC<PropsType>>()
-      .set(PopularFeed, FeedProject)
-      .set(NewFeed, FeedProject);
-    const content = React.createElement(contentMap.get(contentState)!, {
-      total: 8,
-    });
-    return <>{content}</>;
+  const menuHandler = (index: React.SetStateAction<number>) => {
+    setCurrentTab(index);
   };
 
   return (
     <S.Container>
       <S.FeedContent>
         <S.TitleBox>
-          {title.map((value, index) => {
+          {menuArr.map((str, index) => {
             return (
               <S.Title
-                onClick={value.onClick}
+                className={`${
+                  index === currentTab ? "submenu focused" : "submenu"
+                }`}
                 key={index}
-                click={contentState === index + 1}
+                onClick={() => menuHandler(index)}
               >
-                <span>{value.title}</span>
+                {str.name}
               </S.Title>
             );
           })}
         </S.TitleBox>
         <S.ElementBox>
-          <div>{renderContent()}</div>
+          <div>{menuArr[currentTab].content}</div>
         </S.ElementBox>
       </S.FeedContent>
     </S.Container>
