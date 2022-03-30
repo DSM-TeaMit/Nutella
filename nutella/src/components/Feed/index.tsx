755a766d356d1542d8a3349217f22aff3be689c9
kky04994 <kkyo8747@naver.com>
2022-03-30 22:24:11 +0900

    🍫 (#108) -  menu code modification and scroll function

--- nutella/src/components/Feed/index.tsx
+++ nutella/src/components/Feed/index.tsx
@@ -1,20 +1,56 @@
 import * as S from "./styles";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import React from "react";
 import { useFeed } from "../../queries/Feed";
 import Project from "../Cards/MainProjectCard";
+import toast from "react-hot-toast";
+import useInfiniteScroll from "../../hooks/useInfiniteScroll";
+import isMore from "../../constant/IsMore";
 
 const Feed = () => {
   const [currentTab, setCurrentTab] = useState(0);
-  const menuArr = [
-    { name: "인기있는 프로젝트", key: "popularity", content: <Project /> },
-    { name: "최신 프로젝트", key: "recently", content: <Project /> },
-  ];
-  const { data, isError, isSuccess, isLoading } = useFeed();
+  const [page, setPage] = useState<number>(1);
+  const [orderName, setOrderName] = useState<string>("popularity");
+  const { data, isError, isLoading, isFetching } = useFeed(orderName, page);
 
-  const menuHandler = (index: React.SetStateAction<number>) => {
+  const menuHandler = (index: React.SetStateAction<number>, name: string) => {
     setCurrentTab(index);
+    setOrderName(name);
   };
+
+  const menuArr = [
+    {
+      name: "인기있는 프로젝트",
+      key: "popularity",
+      content: <Project data={data?.data?.projects} />,
+    },
+    {
+      name: "최신 프로젝트",
+      key: "recently",
+      content: <Project data={data?.data?.projects} />,
+    },
+  ];
+
+  const onNextPage = () => {
+    if (!data) {
+      return;
+    }
+
+    if (isMore(12, page, data.data.count)) {
+      setPage((prev) => prev + 1);
+    }
+  };
+
+  const ref = useInfiniteScroll<HTMLDivElement>(
+    onNextPage,
+    !(isLoading || isError || isFetching)
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error("가져온 프로젝트가 없습니다.");
+    }
+  }, [isError, orderName]);
 
   return (
     <S.Container>
@@ -27,7 +63,7 @@ const Feed = () => {
                   index === currentTab ? "submenu focused" : "submenu"
                 }`}
                 key={index}
-                onClick={() => menuHandler(index)}
+                onClick={() => menuHandler(index, str.key)}
               >
                 {str.name}
               </S.Title>
@@ -35,7 +71,18 @@ const Feed = () => {
           })}
         </S.TitleBox>
         <S.ElementBox>
-          <S.ProjectBox>{menuArr[currentTab].content}</S.ProjectBox>
+          <S.ProjectBox>
+            {new Array(data?.data.count).fill(0).map((_, index) => (
+              <>{menuArr[currentTab].content}</>
+            ))}
+          </S.ProjectBox>
+          <div ref={ref} />
+          {data?.data.count === 0 && (
+            <>
+              <S.Message>프로젝트가 없습니다.</S.Message>
+              <S.Gap />
+            </>
+          )}
         </S.ElementBox>
       </S.FeedContent>
     </S.Container>
