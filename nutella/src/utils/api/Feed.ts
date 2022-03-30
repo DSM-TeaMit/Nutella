e9ba93f6ac8bba4540cecec9280524efbe8ca3a1
kky04994 <kkyo8747@naver.com>
2022-03-30 22:17:23 +0900

    🍫 (#108) -  add params code and add uuid

--- nutella/src/utils/api/Feed.ts
+++ nutella/src/utils/api/Feed.ts
@@ -1,13 +1,6 @@
 import Uri from "../../constant/Uri";
 import ProjectTypes from "../../interface/ProjectTypes";
 import request from "../axios";
-
-export interface FeedType {
-  order: string;
-  page: number;
-  limit: number;
-}
-
 export interface FeedList {
   thumbnailUrl: string;
   emoji?: string;
@@ -15,13 +8,20 @@ export interface FeedList {
   projectType: ProjectTypes;
   projectField: string;
   viewCount: number;
+  uuid: string;
 }
 
 export interface Feed {
   count: number;
-  projects: FeedList[];
+  projects: FeedList;
 }
 
-export const getFeed = async (data: FeedType) => {
-  return await request.get<Feed>(Uri.projectList.get(), { data });
+export const getFeed = async (order: string, page: number) => {
+  const params = {
+    order: order,
+    page: page,
+    limit: 12,
+  };
+
+  return await request.get<Feed>(Uri.projectList.get(), { params });
 };
