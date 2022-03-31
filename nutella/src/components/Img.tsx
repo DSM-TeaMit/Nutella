7eea26bd702e82430fa9294f09cfc99f36f42fcb
김진근 <freedom7113@gmail.com>
2022-03-31 09:33:19 +0900

    🍫 (#181) - unuse twemoji

--- nutella/src/components/Img.tsx
+++ nutella/src/components/Img.tsx
@@ -1,6 +1,5 @@
 import { FC, useEffect, useMemo } from "react";
 import { useImage } from "../queries/Image";
-import Twemoji from "react-twemoji";
 import toast from "react-hot-toast";
 
 type PropsType = ImageProps & { emoji?: string };
@@ -9,6 +8,47 @@ type ImageProps = React.DetailedHTMLProps<
   React.ImgHTMLAttributes<HTMLImageElement>,
   HTMLImageElement
 >;
+
+function emojiUnicode(input: string) {
+  return emojiUnicode
+    .raw(input)
+    .split(" ")
+    .map((val) => parseInt(val).toString(16))
+    .join(" ");
+}
+
+emojiUnicode.raw = function (input: string) {
+  if (input.length === 1) {
+    return input.charCodeAt(0).toString();
+  } else if (input.length > 1) {
+    const pairs = [];
+    for (let i = 0; i < input.length; i++) {
+      if (
+        // high surrogate
+        input.charCodeAt(i) >= 0xd800 &&
+        input.charCodeAt(i) <= 0xdbff
+      ) {
+        if (
+          input.charCodeAt(i + 1) >= 0xdc00 &&
+          input.charCodeAt(i + 1) <= 0xdfff
+        ) {
+          // low surrogate
+          pairs.push(
+            (input.charCodeAt(i) - 0xd800) * 0x400 +
+              (input.charCodeAt(i + 1) - 0xdc00) +
+              0x10000
+          );
+        }
+      } else if (input.charCodeAt(i) < 0xd800 || input.charCodeAt(i) > 0xdfff) {
+        // modifiers and joiners
+        pairs.push(input.charCodeAt(i));
+      }
+    }
+    return pairs.join(" ");
+  }
+
+  return "";
+};
 
 const Img: FC<PropsType> = (props) => {
   const propsWithoutSomething: Omit<PropsType, "src" | "emoji"> = useMemo(
@@ -21,12 +61,19 @@ const Img: FC<PropsType> = (props) => {
   const { src, emoji } = props;
 
   const { data, isError, isLoading } = useImage(src || "");
+  const unicode = useMemo(
+    () =>
+      emojiUnicode(emoji || "")
+        .split(" ")
+        .join("-"),
+    [emoji]
+  );
 
   useEffect(() => {
-    if (isError && !emoji) {
+    if (isError && !emoji && !src) {
       toast.error("이미지를 가져올 수 없습니다.");
     }
-  }, [emoji, isError]);
+  }, [emoji, isError, src]);
 
   if (isLoading) {
     return <img {...propsWithoutSomething} alt={undefined} />;
@@ -34,15 +81,11 @@ const Img: FC<PropsType> = (props) => {
 
   if (emoji) {
     return (
-      <Twemoji
-        options={{
-          folder: "svg",
-          ext: ".svg",
-          className: `${props.className} default-emoji`,
-        }}
-      >
-        {emoji}
-      </Twemoji>
+      <img
+        {...propsWithoutSomething}
+        alt={undefined}
+        src={`https://twitter.github.io/twemoji/v/13.1.0/svg/${unicode}.svg`}
+      />
     );
   }
 
