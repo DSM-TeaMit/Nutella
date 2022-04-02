2bd45c68e28e43e3e37685a82694bc6675a5ab4b
kky04994 <kkyo8747@naver.com>
2022-04-02 21:27:31 +0900

    🍫 (#176) - add usesearchtype

--- src/queries/Search.ts
+++ src/queries/Search.ts
@@ -1,7 +1,17 @@
 import { useQuery } from "react-query";
 import queryKeys from "../constant/QueryKeys";
-import { getSearch } from "../utils/api/Search";
+import { getSearch, getSearchType } from "../utils/api/Search";
 
 export const useSearch = (keyword: string) => {
   return useQuery([queryKeys.search], () => getSearch(keyword));
+};
+
+export const useSearchType = (
+  page: number,
+  keyword: string,
+  searchType: string
+) => {
+  return useQuery([queryKeys.searchType], () =>
+    getSearchType(page, keyword, searchType)
+  );
 };
