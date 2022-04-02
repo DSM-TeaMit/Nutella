ef6a91c121b7ef5153f881342f5f4ac9e8595da4
kky04994 <kkyo8747@naver.com>
2022-04-02 22:38:42 +0900

    🍫 (#176) - add keyword code on the search queries

--- src/queries/Search.ts
+++ src/queries/Search.ts
@@ -3,7 +3,11 @@ import queryKeys from "../constant/QueryKeys";
 import { getSearch, getSearchType } from "../utils/api/Search";
 
 export const useSearch = (keyword: string) => {
-  return useQuery([queryKeys.search], () => getSearch(keyword));
+  return useQuery([queryKeys.search, keyword], () => getSearch(keyword), {
+    enabled: !!keyword,
+    cacheTime: Infinity,
+    staleTime: Infinity,
+  });
 };
 
 export const useSearchType = (
