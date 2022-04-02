75ba789a0b9a49800d3f4da2e69f25f115b85784
kky04994 <kkyo8747@naver.com>
2022-04-02 22:40:45 +0900

    🍫 (#176) - add search api peristalsis processing code

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -2,13 +2,23 @@ import * as S from "./styles";
 import Project from "../Cards/MainProjectCard";
 import useTitle from "../../hooks/useTitle";
 import { useSearchParams } from "react-router-dom";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
+import { useSearch } from "../../queries/Search";
+import { project } from "../../utils/api/Feed";
+import toast from "react-hot-toast";
 
 const Search = () => {
   const [searchParams] = useSearchParams();
   const searchWord = useMemo(() => searchParams.get("q") || "", [searchParams]);
+  const { data, isError } = useSearch(searchWord);
 
   useTitle(`${searchWord}의 검색결과`);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error("가져온 프로젝트가 없습니다.");
+    }
+  }, [isError]);
 
   return (
     <S.Container>
@@ -16,15 +26,30 @@ const Search = () => {
         <S.SearchTitle>{searchWord}(으)로 검색한 결과</S.SearchTitle>
         <S.ElementBox>
           <S.Title>
-            프로젝트 <span>12개</span>
+            프로젝트 <span>{data?.data.projectName.count}개</span>
           </S.Title>
-          <S.ProjectBox>{/* <Project /> */}</S.ProjectBox>
+          <S.ProjectBox>
+            {data?.data.projectName.projects.map((item: project) => {
+              return <Project key={item.uuid} data={item} />;
+            })}
+            {data?.data.projectName.count === 0 && (
+              <S.Message>검색된 프로젝트가 없습니다.</S.Message>
+            )}
+          </S.ProjectBox>
         </S.ElementBox>
         <S.ElementBox>
           <S.Title>
-            이름에 Tea(을)를 포함한 유저가 참여한 프로젝트 <span>12개</span>
+            이름에 {searchWord}(을)를 포함한 유저가 참여한 프로젝트{" "}
+            <span>{data?.data.memberName.count}개</span>
           </S.Title>
-          <S.ProjectBox>{/* <Project /> */}</S.ProjectBox>
+          <S.ProjectBox>
+            {data?.data.memberName.projects.map((item: project) => {
+              <Project key={item.uuid} data={item} />;
+            })}
+            {data?.data.memberName.count === 0 && (
+              <S.Message>유저가 참여한 프로젝트가 없습니다.</S.Message>
+            )}
+          </S.ProjectBox>
         </S.ElementBox>
       </S.SearchContent>
     </S.Container>
