c8ef4dcab92751f0b8bf488e8de5481964faaaab
kky04994 <kkyo8747@naver.com>
2022-04-03 17:26:12 +0900

    🍫 (#197) - change projectdetails queries name

--- src/queries/ProjectDetails.ts
+++ src/queries/ProjectDetails.ts
@@ -2,5 +2,5 @@ import { useQuery } from "react-query";
 import queryKeys from "../constant/QueryKeys";
 import { getProject } from "../utils/api/ProjectDetails";
 
-export const useComment = (projectUuid: string) =>
+export const useProjectDetails = (projectUuid: string | any) =>
   useQuery([queryKeys.projects], () => getProject(projectUuid));
