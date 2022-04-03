c905472f756eb75105762269a831dcfe81a52986
kky04994 <kkyo8747@naver.com>
2022-04-03 17:37:10 +0900

    🍫 (#196) - add projectdetails api content processing code

--- src/components/ProjectDetail/SubmitContent/index.tsx
+++ src/components/ProjectDetail/SubmitContent/index.tsx
@@ -1,26 +1,32 @@
 import * as S from "./styles";
 import { PlusIcons } from "../../../assets/icons";
+import { Project } from "../../../utils/api/ProjectDetails";
+import { FC } from "react";
 
-const SubmitContent = () => {
+interface PropsType {
+  data: Project | undefined;
+}
+
+const SubmitContent: FC<PropsType> = ({ data }) => {
   return (
     <S.SubmitContentContainer>
       <S.SubmitContent>
         <S.ProjectFile>
-          <S.SubTitle>Teamit 계획서</S.SubTitle>
+          <S.SubTitle>{data?.projectName} 계획서</S.SubTitle>
           <S.SubmitBox>
             <S.PlusBox>
               <img src={PlusIcons} alt="plus" />
             </S.PlusBox>
-            <S.Font>Teamit 계획서 작성하기</S.Font>
+            <S.Font>{data?.projectName} 계획서 작성하기</S.Font>
           </S.SubmitBox>
         </S.ProjectFile>
         <S.ProjectFile>
-          <S.SubTitle>Teamit 결과 보고서</S.SubTitle>
+          <S.SubTitle>{data?.projectName} 결과 보고서</S.SubTitle>
           <S.SubmitBox>
             <S.PlusBox>
               <img src={PlusIcons} alt="plus" />
             </S.PlusBox>
-            <S.Font>Teamit 결과 보고서 작성하기</S.Font>
+            <S.Font>{data?.projectName} 결과 보고서 작성하기</S.Font>
           </S.SubmitBox>
         </S.ProjectFile>
       </S.SubmitContent>
