62347b26b5200050cc38d51f2eeb698f22a4a75d
kky04994 <kkyo8747@naver.com>
2022-04-13 19:11:29 +0900

    😎 (#201) - fix mistake pull from the main branch

--- src/queries/Project.ts
+++ src/queries/Project.ts
@@ -6,18 +6,12 @@ import {
   confirmProjectReport,
   ConfirmType,
   ConfirmValue,
-  deleteProject,
-  modifyProjectInfo,
-  modifyProjectMember,
-  ProjectInfo,
-  ProjectMember,
   createProject,
   Member,
 } from "../utils/api/Project";
 
 export const useConfirmReport = (projectUuid: string, type: ConfirmType) => {
   const queryClient = useQueryClient();
-  
 
   const onSuccess = useCallback(() => {
     queryClient.invalidateQueries(projectUuid);
@@ -33,3 +27,23 @@ export const useConfirmReport = (projectUuid: string, type: ConfirmType) => {
     { onSuccess }
   );
 };
+
+interface CreateProject {
+  name: string;
+  field: string;
+  role: string;
+  type: ProjectTypes;
+  members: Member[];
+}
+
+export const useCreateProject = () =>
+  useMutation((data: CreateProject) =>
+    toast.promise(
+      createProject(data.name, data.field, data.type, data.members, data.role),
+      {
+        error: "프로젝트 생성 중 오류 발생",
+        loading: "프로젝트 생성 중",
+        success: "프로젝트 생성 성공",
+      }
+    )
+  );
