ab1a39122b4860536050c6bf8b2dfb6580cdcb30
kky04994 <kkyo8747@naver.com>
2022-04-15 19:35:22 +0900

    🍫 (#201) - add project deletion interlock code

--- src/components/Modals/ProjectDelete/index.tsx
+++ src/components/Modals/ProjectDelete/index.tsx
@@ -1,11 +1,26 @@
+import toast from "react-hot-toast";
+import { useParams } from "react-router-dom";
 import useModalContext from "../../../hooks/useModalContext";
+import { useProjectDetails } from "../../../queries/ProjectDetails";
 import { RedButton, BorderButton } from "../../Buttons";
 import Input from "../../Input";
+import useInput from "../../../hooks/useInput";
 import * as S from "./styles";
+import { deleteProject } from "../../../utils/api/Project";
 
 const ProjectDeleteModal = () => {
   const { closeCurrentModal } = useModalContext();
-  const onClickDelete = () => {};
+  const { uuid } = useParams<{ uuid: string }>();
+  const { data } = useProjectDetails(uuid);
+  const [inputProps, [value]] = useInput();
+
+  const onClickDelete = async () => {
+    await toast.promise(deleteProject(uuid || ""), {
+      error: `${data?.data.projectName} + ""프로젝트 삭제가 실패되었습니다, 다시 시도해 주세요."`,
+      success: `${data?.data.projectName} + "프로젝트 삭제가 완료되었습니다."`,
+      loading: "삭제 중입니다.",
+    });
+  };
 
   return (
     <S.ProjectDeleteContainer>
@@ -18,10 +33,18 @@ const ProjectDeleteModal = () => {
         <S.ContentText>
           삭제를 진행하시려면 <span>Teamit</span>을 입력해 주세요.
         </S.ContentText>
-        <Input placeholder="Teamit을 입력해 주세요." />
+        <Input
+          {...inputProps}
+          placeholder={`${data?.data.projectName} 을 입력해 주세요.`}
+        />
       </S.ContentBox>
       <S.BtnBox>
-        <RedButton onClick={onClickDelete}>삭제</RedButton>
+        <RedButton
+          disabled={data?.data.projectName !== value}
+          onClick={onClickDelete}
+        >
+          삭제
+        </RedButton>
         <BorderButton onClick={closeCurrentModal}>취소</BorderButton>
       </S.BtnBox>
     </S.ProjectDeleteContainer>
