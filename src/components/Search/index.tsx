c169f294774eb2fbd44d3c09190d39224b6449f6
kky04994 <kkyo8747@naver.com>
2022-04-19 15:25:28 +0900

    🍫 (#176) - process project search interlocking code

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -10,47 +10,64 @@ import toast from "react-hot-toast";
 const Search = () => {
   const [searchParams] = useSearchParams();
   const searchWord = useMemo(() => searchParams.get("q") || "", [searchParams]);
-  const { data, isError } = useSearch(searchWord);
+  const { data, isError, isLoading } = useSearch(searchWord);
 
   useTitle(`${searchWord}의 검색결과`);
 
   useEffect(() => {
     if (isError) {
-      toast.error("가져온 프로젝트가 없습니다.");
+      toast.error("오류가 발생했습니다. 다시 시도해 주세요");
     }
   }, [isError]);
+
+  if (isLoading) {
+    return;
+  }
 
   return (
     <S.Container>
       <S.SearchContent>
         <S.SearchTitle>{searchWord}(으)로 검색한 결과</S.SearchTitle>
-        <S.ElementBox>
-          <S.Title>
-            프로젝트 <span>{data?.data.projectName.count}개</span>
-          </S.Title>
-          <S.ProjectBox>
-            {data?.data.projectName.projects.map((item: project) => {
-              return <Project key={item.uuid} data={item} />;
-            })}
-            {data?.data.projectName.count === 0 && (
-              <S.Message>검색된 프로젝트가 없습니다.</S.Message>
-            )}
-          </S.ProjectBox>
-        </S.ElementBox>
-        <S.ElementBox>
-          <S.Title>
-            이름에 {searchWord}(을)를 포함한 유저가 참여한 프로젝트{" "}
-            <span>{data?.data.memberName.count}개</span>
-          </S.Title>
-          <S.ProjectBox>
-            {data?.data.memberName.projects.map((item: project) => {
-              <Project key={item.uuid} data={item} />;
-            })}
-            {data?.data.memberName.count === 0 && (
-              <S.Message>유저가 참여한 프로젝트가 없습니다.</S.Message>
-            )}
-          </S.ProjectBox>
-        </S.ElementBox>
+        {data?.data.projectName.count === 0 ||
+        data?.data.memberName.count === 0 ? (
+          <>
+            <S.BigMessage>:(</S.BigMessage>
+            <S.BigMessage>
+              {searchWord}(으)로 검색한 결과가 존재하지 않습니다.
+            </S.BigMessage>
+            <S.BigMessage>다른 키워드로 검색해 주세요.</S.BigMessage>
+          </>
+        ) : (
+          <>
+            <S.ElementBox>
+              <S.Title>
+                프로젝트 <span>{data?.data.projectName.count}개</span>
+              </S.Title>
+              <S.ProjectBox>
+                {data?.data.projectName.projects.map((item: project) => {
+                  return <Project key={item.uuid} data={item} />;
+                })}
+                {data?.data.projectName.count === 0 && (
+                  <S.Message>검색된 프로젝트가 없습니다.</S.Message>
+                )}
+              </S.ProjectBox>
+            </S.ElementBox>
+            <S.ElementBox>
+              <S.Title>
+                이름에 {searchWord}(을)를 포함한 유저가 참여한 프로젝트{" "}
+                <span>{data?.data.memberName.count}개</span>
+              </S.Title>
+              <S.ProjectBox>
+                {data?.data.memberName.projects.map((item: project) => {
+                  <Project key={item.uuid} data={item} />;
+                })}
+                {data?.data.memberName.count === 0 && (
+                  <S.Message>유저가 참여한 프로젝트가 없습니다.</S.Message>
+                )}
+              </S.ProjectBox>
+            </S.ElementBox>
+          </>
+        )}
       </S.SearchContent>
     </S.Container>
   );
