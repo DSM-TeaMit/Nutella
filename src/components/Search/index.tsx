cd950d1d240c8d0b807efc6e472b8c98d9723b23
kky04994 <kkyo8747@naver.com>
2022-04-20 22:21:00 +0900

    🍫 (#176) - add skeleton code when loading

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -6,6 +6,7 @@ import { useEffect, useMemo } from "react";
 import { useSearch } from "../../queries/Search";
 import { project } from "../../utils/api/Feed";
 import toast from "react-hot-toast";
+import MainProjectSkeleton from "../Cards/MainProjectSkeleton";
 
 const Search = () => {
   const [searchParams] = useSearchParams();
@@ -20,15 +21,19 @@ const Search = () => {
     }
   }, [isError]);
 
-  if (isLoading) {
-    return;
-  }
+  const skeletons = useMemo(
+    () =>
+      Array(4)
+        .fill(0)
+        .map((_, index) => <MainProjectSkeleton key={index} />),
+    []
+  );
 
   return (
     <S.Container>
       <S.SearchContent>
         <S.SearchTitle>{searchWord}(으)로 검색한 결과</S.SearchTitle>
-        {data?.data.projectName.count === 0 ||
+        {data?.data.projectName.count === 0 &&
         data?.data.memberName.count === 0 ? (
           <>
             <S.BigMessage>:(</S.BigMessage>
@@ -40,27 +45,37 @@ const Search = () => {
         ) : (
           <>
             <S.ElementBox>
-              <S.Title>
-                프로젝트 <span>{data?.data.projectName.count}개</span>
-              </S.Title>
+              {isLoading ? null : (
+                <S.Title>
+                  프로젝트 <span>{data?.data.projectName.count}개</span>
+                </S.Title>
+              )}
+
               <S.ProjectBox>
-                {data?.data.projectName.projects.map((item: project) => {
-                  return <Project key={item.uuid} data={item} />;
-                })}
+                {isLoading
+                  ? skeletons
+                  : data?.data.projectName.projects.map((item: project) => {
+                      return <MainProjectCard key={item.uuid} data={item} />;
+                    })}
                 {data?.data.projectName.count === 0 && (
                   <S.Message>검색된 프로젝트가 없습니다.</S.Message>
                 )}
               </S.ProjectBox>
             </S.ElementBox>
             <S.ElementBox>
-              <S.Title>
-                이름에 {searchWord}(을)를 포함한 유저가 참여한 프로젝트{" "}
-                <span>{data?.data.memberName.count}개</span>
-              </S.Title>
+              {isLoading ? null : (
+                <S.Title>
+                  이름에 {searchWord}(을)를 포함한 유저가 참여한 프로젝트{" "}
+                  <span>{data?.data.memberName.count}개</span>
+                </S.Title>
+              )}
               <S.ProjectBox>
-                {data?.data.memberName.projects.map((item: project) => {
-                  <Project key={item.uuid} data={item} />;
-                })}
+                {isLoading
+                  ? skeletons
+                  : data?.data.memberName.projects.map((item: project) => {
+                      return <MainProjectCard key={item.uuid} data={item} />;
+                    })}
+
                 {data?.data.memberName.count === 0 && (
                   <S.Message>유저가 참여한 프로젝트가 없습니다.</S.Message>
                 )}
