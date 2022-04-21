ef62144ceb16b3db473a535038bde1227d37d967
kky04994 <kkyo8747@naver.com>
2022-04-21 23:44:33 +0900

    🍫 (#213) - add uploadthumbanil queries code

--- src/queries/ProjectDetails.ts
+++ src/queries/ProjectDetails.ts
@@ -1,6 +1,15 @@
-import { useQuery } from "react-query";
+import toast from "react-hot-toast";
+import { useMutation, useQuery } from "react-query";
 import queryKeys from "../constant/QueryKeys";
-import { getProject } from "../utils/api/ProjectDetails";
+import { getProject, uploadImg } from "../utils/api/ProjectDetails";
 
 export const useProjectDetails = (projectUuid: string | any) =>
   useQuery([queryKeys.projects], () => getProject(projectUuid));
+
+export const useUploadThumbnail = (image: string) => {
+  toast.promise(uploadImg(image), {
+    error: "업로드가 실패했습니다.",
+    loading: "사진 업로드 로딩 중",
+    success: "사진 업로드가 성공했습니다.",
+  });
+};
