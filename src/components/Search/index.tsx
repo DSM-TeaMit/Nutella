e427a09de57f628fed56cbdc5c00ded42814f370
kky04994 <kkyo8747@naver.com>
2022-04-24 23:11:39 +0900

    🍫 (#176) - add processing code according to pagination

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -2,16 +2,24 @@ import * as S from "./styles";
 import MainProjectCard from "../Cards/MainProjectCard";
 import useTitle from "../../hooks/useTitle";
 import { useSearchParams } from "react-router-dom";
-import { useEffect, useMemo } from "react";
-import { useSearch } from "../../queries/Search";
+import { useEffect, useMemo, useState } from "react";
+import { useSearch, useSearchType } from "../../queries/Search";
 import { project } from "../../utils/api/Feed";
 import toast from "react-hot-toast";
 import MainProjectSkeleton from "../Cards/MainProjectSkeleton";
+import Pagination from "./Pagination";
 
 const Search = () => {
   const [searchParams] = useSearchParams();
   const searchWord = useMemo(() => searchParams.get("q") || "", [searchParams]);
   const { data, isError, isLoading } = useSearch(searchWord);
+  const [memberPage, setMemberPage] = useState<number>(0);
+  const [projectPage, setProjectPage] = useState<number>(0);
+  const { data: SearchData } = useSearchType(
+    memberPage,
+    searchWord,
+    "ProjectName"
+  );
 
   useTitle(`${searchWord}의 검색결과`);
 
@@ -21,13 +29,23 @@ const Search = () => {
     }
   }, [isError]);
 
-  const skeletons = useMemo(
-    () =>
-      Array(4)
-        .fill(0)
-        .map((_, index) => <MainProjectSkeleton key={index} />),
-    []
-  );
+  useEffect(() => {
+    setMemberPage(Number(data?.data.memberName.count) / 4);
+    setProjectPage(Number(data?.data.projectName.count) / 4);
+    console.log(memberPage);
+    console.log(projectPage);
+  }, [
+    data?.data.memberName.count,
+    data?.data.projectName.count,
+    memberPage,
+    projectPage,
+  ]);
+
+  const skeletons = Array<number>(4)
+    .fill(0)
+    .map((_, index) => {
+      return <MainProjectSkeleton key={index} />;
+    });
 
   return (
     <S.Container>
@@ -61,6 +79,11 @@ const Search = () => {
                   <S.Message>검색된 프로젝트가 없습니다.</S.Message>
                 )}
               </S.ProjectBox>
+              <Pagination
+                type={"projectName"}
+                total={memberPage}
+                keyword={searchWord}
+              />
             </S.ElementBox>
             <S.ElementBox>
               {isLoading ? null : (
@@ -80,6 +103,11 @@ const Search = () => {
                   <S.Message>유저가 참여한 프로젝트가 없습니다.</S.Message>
                 )}
               </S.ProjectBox>
+              <Pagination
+                type={"memberName"}
+                total={memberPage}
+                keyword={searchWord}
+              />
             </S.ElementBox>
           </>
         )}
