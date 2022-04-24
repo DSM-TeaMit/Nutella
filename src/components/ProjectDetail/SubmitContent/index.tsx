c6c1069c52994627f8f23a8a795b5a29feef1451
kky04994 <kkyo8747@naver.com>
2022-04-24 13:28:47 +0900

    😎 (#213) - fix 404 error when into the plan

--- src/components/ProjectDetail/SubmitContent/index.tsx
+++ src/components/ProjectDetail/SubmitContent/index.tsx
@@ -4,7 +4,8 @@ import { Project } from "../../../utils/api/ProjectDetails";
 import { FC, useEffect } from "react";
 import { useCreatePlanMutation } from "../../../queries/Plan";
 import toast from "react-hot-toast";
-import ProjectCard from "../../Cards/ProjectCard";
+import { useNavigate } from "react-router-dom";
+import ReportCard from "../../Cards/ReportCard";
 
 interface PropsType {
   data: Project | undefined;
@@ -14,6 +15,37 @@ const SubmitContent: FC<PropsType> = ({ data }) => {
   useEffect(() => {}, []);
   const planMutation = useCreatePlanMutation(data?.uuid || "");
   const resultMutation = useCreatePlanMutation(data?.uuid || "");
+  const navigate = useNavigate();
+
+  const onPlanSuccess = () => {
+    navigate(`/project/${data?.uuid}/plan`);
+  };
+
+  const onPlanError = () => {
+    toast.error("계획서 이동하려는 도중 오류가 났습니다. 다시 시도해 주세요.");
+  };
+
+  const onClickPlan = () => {
+    planMutation.mutate(undefined, {
+      onSuccess: onPlanSuccess,
+      onError: onPlanError,
+    });
+  };
+
+  const onResultSuccess = () => {
+    navigate(`/project/${data?.uuid}/result`);
+  };
+
+  const onResultError = () => {
+    toast.error("보고서 이동하는 도중 오류가 났습니다. 다시 시도해 주세요.");
+  };
+
+  const onClickResult = () => {
+    resultMutation.mutate(undefined, {
+      onSuccess: onResultSuccess,
+      onError: onResultError,
+    });
+  };
 
   return (
     <S.SubmitContentContainer>
@@ -22,16 +54,11 @@ const SubmitContent: FC<PropsType> = ({ data }) => {
           <S.SubTitle>{data?.projectName} 계획서</S.SubTitle>
           <S.SubmitBox>
             {data?.projectStatus === "REPORTING" ? (
+              //<ReportCard />
               <div>계획서 카드 보여주는 곳</div>
             ) : (
-              // <ProjectCard data={data}></ProjectCard>
               <>
-                <S.SubmitLinkBox
-                  to={`/project/${data?.uuid}/plan`}
-                  onClick={() => {
-                    planMutation.mutate();
-                  }}
-                >
+                <S.SubmitLinkBox onClick={() => onClickPlan()}>
                   <S.PlusBox>
                     <img src={PlusIcons} alt="plus" />
                   </S.PlusBox>
@@ -50,7 +77,7 @@ const SubmitContent: FC<PropsType> = ({ data }) => {
                   onClick={() => {
                     resultMutation.mutate();
                   }}
-                  to={`/project/${data?.uuid}/result`}
+                  // to=
                   check={false}
                 />
                 <div>
@@ -64,10 +91,9 @@ const SubmitContent: FC<PropsType> = ({ data }) => {
               <>
                 <S.GrayBox
                   onClick={() => toast.error("계획서부터!")}
-                  to=""
                   check={true}
                 >
-                  <S.Font>계획서를 먼저 작성해 주세요.</S.Font>
+                  <S.Font>계획서부터 먼저 작성해 주세요.</S.Font>
                 </S.GrayBox>
               </>
             )}
