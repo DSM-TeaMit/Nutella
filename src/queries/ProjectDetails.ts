02467897881f89ba0e99be117ff9ea8bef3f777f
kky04994 <kkyo8747@naver.com>
2022-04-25 11:12:15 +0900

    🍫 (#221) - change useuploadthumbnail code

--- src/queries/ProjectDetails.ts
+++ src/queries/ProjectDetails.ts
@@ -1,15 +1,10 @@
-import toast from "react-hot-toast";
 import { useMutation, useQuery } from "react-query";
 import queryKeys from "../constant/QueryKeys";
 import { getProject, uploadImg } from "../utils/api/ProjectDetails";
 
-export const useProjectDetails = (projectUuid: string | any) =>
-  useQuery([queryKeys.projects], () => getProject(projectUuid));
+export const useProjectDetails = (projectUuid: string) =>
+  useQuery([queryKeys.projects, projectUuid], () => getProject(projectUuid));
 
-export const useUploadThumbnail = (image: string) => {
-  toast.promise(uploadImg(image), {
-    error: "업로드가 실패했습니다.",
-    loading: "사진 업로드 로딩 중",
-    success: "사진 업로드가 성공했습니다.",
-  });
+export const useUploadThumbnail = (projectUuid: string) => {
+  return useMutation((image: File) => uploadImg(projectUuid, image));
 };
