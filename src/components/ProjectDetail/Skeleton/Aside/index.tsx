1d48b7517dd95afcf87fd53775a40328e984c00e
kky04994 <kkyo8747@naver.com>
2022-04-27 01:58:34 +0900

    🍫 (#220) - add asideskeleton file

---
+++ src/components/ProjectDetail/Skeleton/Aside/index.tsx
@@ -0,0 +1,32 @@
+import { Fragment } from "react";
+import * as S from "./styles";
+
+const AsideSkeleton = () => {
+  return (
+    <Fragment>
+      <S.AsideContainer>
+        <S.AsideTop>
+          <S.Title />
+          <S.SubTitle />
+        </S.AsideTop>
+        <S.AsideContent>
+          {Array(4)
+            .fill(0)
+            .map((_, index) => (
+              <>
+                <S.RoleBox>
+                  <S.RoleTitle />
+                  <S.User>
+                    <div />
+                    <div></div>
+                  </S.User>
+                </S.RoleBox>
+              </>
+            ))}
+        </S.AsideContent>
+      </S.AsideContainer>
+    </Fragment>
+  );
+};
+
+export default AsideSkeleton;
---
+++ src/components/ProjectDetail/Skeleton/Aside/styles.ts
@@ -0,0 +1,67 @@
+import styled from "@emotion/styled";
+
+export const AsideContainer = styled.div`
+  width: 302px;
+  height: 100%;
+  background: ${({ theme }) => theme.colors.grayscale.white};
+  border-radius: 10px;
+  padding: 20px 16px 16px;
+  border: 1px solid ${({ theme }) => theme.colors.grayscale.lightGray1};
+`;
+
+export const AsideTop = styled.div`
+  width: 273px;
+  height: 30px;
+  text-align: center;
+  display: fixed;
+  justify-content: space-between;
+  border-radius: 10px;
+`;
+
+export const Title = styled.div`
+  width: 53px;
+  height: 29px;
+  background-color: ${({ theme }) => theme.colors.grayscale.gray1};
+`;
+
+export const SubTitle = styled.div`
+  width: 30px;
+  height: 29px;
+  background-color: ${({ theme }) => theme.colors.grayscale.gray1};
+`;
+
+export const AsideContent = styled.div`
+  width: 273px;
+  margin-top: 17px;
+`;
+
+export const RoleBox = styled.div`
+  margin-bottom: 36px;
+`;
+
+export const RoleTitle = styled.div`
+  width: 78px;
+  height: 23px;
+  background-color: ${({ theme }) => theme.colors.grayscale.gray1};
+  margin-bottom: 12px;
+`;
+
+export const User = styled.div`
+  display: fixed;
+  height: 40px;
+  padding: 4px;
+  text-decoration: none;
+  user-select: none;
+  div:nth-child(1) {
+    width: 32px;
+    height: 32px;
+    border-radius: 25px;
+    vertical-align: middle;
+    margin-right: 10px;
+  }
+  div {
+    background-color: ${({ theme }) => theme.colors.grayscale.gray1};
+    width: 84px;
+    height: 23px;
+  }
+`;
