6f3b7c26c09866b935da21f2a8a69c50a68d767c
kky04994 <kkyo8747@naver.com>
2022-04-28 01:50:31 +0900

    🍫 (#201) - add changememberrole style code

--- src/components/MemberInput/styles.ts
+++ src/components/MemberInput/styles.ts
@@ -132,3 +132,54 @@ export const Remove = styled.button`
   color: ${({ theme }) => theme.colors.red.default};
   cursor: pointer;
 `;
+
+export const MemberBox = styled.div`
+  height: fit-content;
+`;
+
+export const MemberContent = styled.div`
+  margin-top: 30px;
+  div:nth-child(1) {
+    margin-bottom: none;
+  }
+`;
+
+export const MemberProfile = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 12px;
+`;
+
+export const Delete = styled.span`
+  font: ${({ theme }) => theme.fonts.body3};
+  color: ${({ theme }) => theme.colors.red.default};
+  cursor: pointer;
+`;
+
+export const User = styled.div`
+  display: fixed;
+  height: 32px;
+  img {
+    width: 32px;
+    height: 32px;
+    background: grey;
+    border-radius: 25px;
+    vertical-align: middle;
+    margin-right: 10px;
+  }
+  span {
+    font: ${({ theme }) => theme.fonts.body3};
+    color: ${({ theme }) => theme.colors.grayscale.gray2};
+  }
+`;
+
+export const RollBox = styled.div`
+  display: flex;
+  padding: 10px 16px;
+  border-radius: 12px;
+  flex-direction: column;
+  row-gap: 12px;
+  width: 100%;
+  margin-bottom: 19px;
+  background: ${({ theme }) => theme.colors.grayscale.lightGray1};
+`;
