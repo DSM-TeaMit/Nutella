7333ef6821f6b6ab76dda4c9da5c747db82c73d9
kky04994 <kkyo8747@naver.com>
2022-04-27 10:16:31 +0900

    🍫 (#220) - add topskeleton file

---
+++ src/components/ProjectDetail/Skeleton/Top/index.tsx
@@ -0,0 +1,39 @@
+import { Fragment } from "react";
+import * as S from "./styles";
+
+const TopSkeleton = () => {
+  return (
+    <Fragment>
+      <S.TopContainer>
+        <S.TopContent>
+          <S.ProjectImgBox></S.ProjectImgBox>
+          <div>
+            <S.ProjectTop>
+              <S.ProjectName />
+              <S.ProjectRincian>
+                <S.ViewBox />
+                <S.IconBox />
+                <S.ModifyBox />
+              </S.ProjectRincian>
+            </S.ProjectTop>
+            <S.ProjectContent />
+            <S.ProjectBottom>
+              <div>
+                {Array(4)
+                  .fill(0)
+                  .map((_, index) => (
+                    <>
+                      <S.Field />
+                    </>
+                  ))}
+              </div>
+              <S.Step />
+            </S.ProjectBottom>
+          </div>
+        </S.TopContent>
+      </S.TopContainer>
+    </Fragment>
+  );
+};
+
+export default TopSkeleton;
